23d97c232d7b2d17c7297d2fd857e06a8551d8b4
mpanalig <mpanalig@pcmall.com>
2012-03-27 20:40:57 +0800

    added option to change style of short paging

--- searchManager/WebContent/js/jquery/jquery.pagination.custom.js
+++ searchManager/WebContent/js/jquery/jquery.pagination.custom.js
@@ -1,39 +1,118 @@
 (function($){
-	$.fn.paginate = function(options){
 
-		$.fn.paginate.defaults = {
-				type: "long",
-				currentPage:1,
-				pageSize:10, 
-				totalItem:10,
-				totalPages: 0,
-				shortPagingStyle: "contentStyle",
-				callbackText: function(itemStart, itemEnd, itemTotal){
-					return 'Displaying ' + itemStart + ' to ' + itemEnd + ' of ' + itemTotal + " Items";
-				},
-				pageLinkCallback: function(e){}, 
-				firstLinkCallback: function(e){}, 
-				nextLinkCallback: function(e){}, 
-				prevLinkCallback: function(e){},
-				lastLinkCallback: function(e){},
-				pageChangeCallback: function(e){}
+	$.paginate = function(el, options){
+		// To avoid scope issues, use 'base' instead of 'this'
+		// to reference this class from internal events and functions.
+		var base = this;
+
+		// Access to jQuery and DOM versions of element
+		base.$el = $(el);
+		base.el = el;
+
+		// Add a reverse reference to the DOM object
+		base.$el.data("paginate", base);
+
+		base.init = function(){
+			base.options = $.extend({},$.paginate.defaultOptions, options);
+			
+			var itemStart = (((base.options.currentPage-1)*base.options.pageSize)+1);
+			var itemEnd = (((base.options.currentPage*base.options.pageSize) > base.options.totalItem) ? base.options.totalItem : (base.options.currentPage*base.options.pageSize));
+			base.options.totalPages = Math.ceil(base.options.totalItem/base.options.pageSize);
+
+			var typeOfPaging = $.trim(base.options.type).toLowerCase();
+			
+			if (typeOfPaging === "short") base.useShortPaging(itemStart, itemEnd);
+			if (typeOfPaging === "long") base.useLongPaging(itemStart, itemEnd);
+			
 		};
 
-		var opts = $.extend({}, $.fn.paginate.defaults, options);
+		base.useShortPaging = function(itemStart, itemEnd){
 
-		getLongPaging = function(itemStart, itemEnd){
-			var windowFrom = opts.currentPage - 1;
-			var windowTo = opts.currentPage + 1;
+			var shortPaging = "";
+			var style = $.trim(base.options.pageStyle).toLowerCase();
+			
+			if (style === "style1"){
+				shortPaging += '<div class="txtDisplay styleB" >';
+				shortPaging += base.options.callbackText(itemStart, itemEnd, base.options.totalItem);
+				shortPaging += '</div>';
+
+				shortPaging += '<div class="pagingArrow styleB floatR w160 txtAR padTB5 marT0">';
+				shortPaging += '<a id="firstPage" href="javascript:void(0)" class="btnGraph"><div class="btnGraph floatL btnBlueFirst marT3"></div></a>';
+				shortPaging += '<a id="prevPage" href="javascript:void(0)" class="btnGraph"><div class="btnGraph floatL btnBluePrev mar3"></div></a>';
+				shortPaging += '<span class="padLR3 fsize11 floatL marT3">Page </span>';
+				shortPaging += '<span class="padLR3 fsize11 floatL marT3"><input id="currentPage" type="text" class="w30 fsize11 txtAR floatL marR3 posRel" style="height:15px; top:-3px" value="' + base.options.currentPage + '"> of ' + base.options.totalPages + '</span>';
+				shortPaging += '<a id="nextPage" href="javascript:void(0)" class="btnGraph"><div class="btnGraph floatL btnBlueNext mar3"></div></a>';
+				shortPaging += '<a id="lastPage" href="javascript:void(0)" class="btnGraph"><div class="btnGraph floatL btnBlueLast marT3"></div></a>';
+				shortPaging += "</div>";
+			}
+
+			if (style === "style2"){
+				shortPaging += '<div class="txtDisplay styleB" >';
+				shortPaging += base.options.callbackText(itemStart, itemEnd, base.options.totalItem);
+				shortPaging += '</div>';
+
+				shortPaging += '<div class="pagingArrow styleA floatR w160 txtAR padTB5 marT0 marR5">';
+				shortPaging += '<a id="firstPage" href="javascript:void(0)" class="btnGraph"><div class="btnGraph floatL styleAFirst marT3"></div></a>';
+				shortPaging += '<a id="prevPage" href="javascript:void(0)" class="btnGraph"><div class="btnGraph floatL styleAPrev mar3"></div></a>';
+				shortPaging += '<span class="padLR3 fsize11 floatL marT3">Page </span>';
+				shortPaging += '<span class="padLR3 fsize11 floatL marT3"><input id="currentPage" type="text" class="w30 fsize11 txtAR floatL marR3 posRel" style="height:15px; top:-3px" value="' + base.options.currentPage + '"> of ' + base.options.totalPages + '</span>';
+				shortPaging += '<a id="nextPage" href="javascript:void(0)" class="btnGraph"><div class="btnGraph floatL styleANext mar3"></div></a>';
+				shortPaging += '<a id="lastPage" href="javascript:void(0)" class="btnGraph"><div class="btnGraph floatL styleALast marT3"></div></a>';
+				shortPaging += '</div>';
+			}
+
+			$shortPaging = $(shortPaging);
+
+			var $firstPage = $shortPaging.find("a#firstPage");
+			var $prevPage  = $shortPaging.find("a#prevPage");
+			var $nextPage  = $shortPaging.find("a#nextPage");
+			var $lastPage  = $shortPaging.find("a#lastPage");
+			var $currPage  = $shortPaging.find("input#currentPage");
+
+			if (base.options.totalPages == base.options.currentPage){
+				$nextPage.off();
+				$lastPage.off();
+			}else{
+				$nextPage.on({click:base.options.nextLinkCallback}, { page:base.options.currentPage});
+				$lastPage.on({click:base.options.lastLinkCallback}, { page:base.options.currentPage, totalPages:base.options.totalPages});
+			}
+
+			if (base.options.currentPage == 1){
+				$firstPage.off();
+				$prevPage.off();
+			}else{
+				$firstPage.on({click:base.options.firstLinkCallback}, { page:base.options.currentPage});
+				$prevPage.on({click:base.options.prevLinkCallback}, { page:base.options.currentPage});
+			}
+
+			$currPage.on(
+					{	keypress:base.jumpToPage,
+						focus: function(e){
+							$(e.target).val("");
+						},
+						blur: function(e){
+							$(e.target).val(base.options.currentPage);
+						}},
+						{	currentPage:base.options.currentPage, 
+							totalPages:base.options.totalPages }
+			);
+
+			return base.$el.append($shortPaging);
+		};
+
+		base.useLongPaging = function(itemStart, itemEnd){
+			var windowFrom = base.options.currentPage - 1;
+			var windowTo = base.options.currentPage + 1;
 			var outerWindow = 1;
 			var longPaging = "";
 
 			// If the window is truncated on one side, make the other side longer
-			if (windowTo > opts.totalPages) {
-				windowFrom = Math.max(0, windowFrom - (windowTo - opts.totalPages));
-				windowTo = opts.totalPages;
+			if (windowTo > base.options.totalPages) {
+				windowFrom = Math.max(0, windowFrom - (windowTo - base.options.totalPages));
+				windowTo = base.options.totalPages;
 			}
 			if (windowFrom < 1) {
-				windowTo = Math.min(opts.totalPages, windowTo + (1 - windowFrom));
+				windowTo = Math.min(base.options.totalPages, windowTo + (1 - windowFrom));
 				windowFrom = 1;
 			}
 
@@ -50,24 +129,24 @@
 				visible.push(windowFrom - 1);
 			}
 			// Don't add the first or last page twice
-			for (var i = Math.max(2, windowFrom); i <= Math.min(windowTo, opts.totalPages - 1); i++) {
+			for (var i = Math.max(2, windowFrom); i <= Math.min(windowTo, base.options.totalPages - 1); i++) {
 				visible.push(i);
 			}
 			// If the gap is just one page, close the gap
-			if (opts.totalPages - outerWindow == windowTo + 2) {
+			if (base.options.totalPages - outerWindow == windowTo + 2) {
 				visible.push(windowTo + 1);
 			}
 			// Don't add inner window pages twice
-			for (var i = Math.max(opts.totalPages - outerWindow, windowTo + 1); i < opts.totalPages; i++) {
+			for (var i = Math.max(base.options.totalPages - outerWindow, windowTo + 1); i < base.options.totalPages; i++) {
 				visible.push(i);
 			}
 			// Always show the last page, unless it's the first page
-			if (opts.totalPages > 1) {
-				visible.push(opts.totalPages);
+			if (base.options.totalPages > 1) {
+				visible.push(base.options.totalPages);
 			}
 
 			longPaging += '<div class="txtDisplay floatL farial fsize11 fDblue padT10 marL8">';
-			longPaging += opts.callbackText(itemStart, itemEnd, opts.totalItem);
+			longPaging += base.options.callbackText(itemStart, itemEnd, base.options.totalItem);
 			longPaging += '</div>';
 			longPaging += '<div class="floatR farial fsize11 fgray txtAR padT10">';
 			longPaging += '	<div class="txtAR">';
@@ -82,22 +161,22 @@
 
 			for (var i = 0, l = visible.length; i < l; i++) {
 				if (prev && visible[i] > prev + 1) links.push("<li><span>&hellip;</span></li>");
-				var activePage = (opts.currentPage == visible[i])? "pager-current" : "";
+				var activePage = (base.options.currentPage == visible[i])? "pager-current" : "";
 				var $listItem = $('<li><span class="' + activePage + '"><a href="javascript:void(0);">' + visible[i] + '</a></span></li>');
-				$listItem.find("a").on({click:opts.pageLinkCallback}, { page:visible[i] });
+				$listItem.find("a").on({click:base.options.pageLinkCallback}, { page:visible[i] });
 				links.push($listItem);
 				prev = visible[i];
 			}
 
-			if (opts.currentPage != opts.totalPages){
+			if (base.options.currentPage != base.options.totalPages){
 				var $nextLink = $('<li><a href="javascript:void(0);">Next &raquo;</a></li>');
-				$nextLink.find("a").on({click:opts.nextLinkCallback}, { page:opts.currentPage });
+				$nextLink.find("a").on({click:base.options.nextLinkCallback}, { page:base.options.currentPage });
 				links.push($nextLink);
 			}
 
-			if (opts.currentPage > 1){
+			if (base.options.currentPage > 1){
 				var $prevLink = $('<li><a href="javascript:void(0);">Prev &raquo;</a></li>');
-				$prevLink.find("a").on({click:opts.prevLinkCallback}, { page:opts.currentPage });
+				$prevLink.find("a").on({click:base.options.prevLinkCallback}, { page:base.options.currentPage });
 				links.unshift($prevLink);
 			}
 
@@ -105,75 +184,16 @@
 				$longPaging.find("ul.pagination").append(links[i]);
 			}
 
-			return $longPaging;
-		};
-
-		getShortPaging = function(itemStart, itemEnd){
-			var shortPaging = '<div class="txtDisplay floatL farial fsize11 fDblue padT10 marL10" >';
-			shortPaging += opts.callbackText(itemStart, itemEnd, opts.totalItem);
-			shortPaging += '</div>';
-			
-			if (opts.shortPagingStyle == "leftStyle"){
-				shortPaging += '<div class="pagingArrow floatR w160 txtAR padTB5 marT0">';
-				shortPaging += '<a id="firstPage" href="javascript:void(0)" class="btnGraph"><div class="btnGraph floatL btnBlueFirst marT3"></div></a>';
-			    shortPaging += '<a id="prevPage" href="javascript:void(0)" class="btnGraph"><div class="btnGraph floatL btnBluePrev mar3"></div></a>';
-			    shortPaging += '<span class="fDblue padLR3 fsize11 floatL marT3">Page </span>';
-			    shortPaging += '<span class="fDblue padLR3 fsize11 floatL marT3"><input id="currentPage" type="text" class="w30 fsize11 txtAR floatL marR3 posRel" style="height:15px; top:-3px" value="' + opts.currentPage + '"> of ' + opts.totalPages + '</span>';
-			    shortPaging += '<a id="nextPage" href="javascript:void(0)" class="btnGraph"><div class="btnGraph floatL btnBlueNext mar3"></div></a>';
-			    shortPaging += '<a id="lastPage" href="javascript:void(0)" class="btnGraph"><div class="btnGraph floatL btnBlueLast marT3"></div></a>';
-				shortPaging += "</div>";
-			}
-			
-			if (opts.shortPagingStyle == "contentStyle"){
-				shortPaging += '<div class="pagingArrow floatR w160 txtAR padTB5 marT0">';
-				shortPaging += '<a id="firstPage" href="javascript:void(0)" class="btnGraph"><div class="btnGraph floatL btnBlueFirst marT3"></div></a>';
-			    shortPaging += '<a id="prevPage" href="javascript:void(0)" class="btnGraph"><div class="btnGraph floatL btnBluePrev mar3"></div></a>';
-			    shortPaging += '<span class="fDblue padLR3 fsize11 floatL marT3">Page JJJJ </span>';
-			    shortPaging += '<span class="fDblue padLR3 fsize11 floatL marT3"><input id="currentPage" type="text" class="w30 fsize11 txtAR floatL marR3 posRel" style="height:15px; top:-3px" value="' + opts.currentPage + '"> of ' + opts.totalPages + '</span>';
-			    shortPaging += '<a id="nextPage" href="javascript:void(0)" class="btnGraph"><div class="btnGraph floatL btnBlueNext mar3"></div></a>';
-			    shortPaging += '<a id="lastPage" href="javascript:void(0)" class="btnGraph"><div class="btnGraph floatL btnBlueLast marT3"></div></a>';
-				shortPaging += "</div>";
-			}
-			
-			$shortPaging = $(shortPaging);
-			
-			var $firstPage = $shortPaging.find("a#firstPage");
-			var $prevPage  = $shortPaging.find("a#prevPage");
-			var $nextPage  = $shortPaging.find("a#nextPage");
-			var $lastPage  = $shortPaging.find("a#lastPage");
-			var $currPage  = $shortPaging.find("input#currentPage");
-			
-			if (opts.totalPages == opts.currentPage){
-				$nextPage.off();
-				$lastPage.off();
-			}else{
-				$nextPage.on({click:opts.nextLinkCallback}, { page:opts.currentPage});
-				$lastPage.on({click:opts.lastLinkCallback}, { page:opts.currentPage, totalPages:opts.totalPages});
-			}
-			
-			if (opts.currentPage == 1){
-				$firstPage.off();
-				$prevPage.off();
-			}else{
-				$firstPage.on({click:opts.firstLinkCallback}, { page:opts.currentPage});
-				$prevPage.on({click:opts.prevLinkCallback}, { page:opts.currentPage});
-			}
-			
-			$currPage.on({
-				keypress:jumpToPage,
-				focus: function(e){$(e.target).val("");},
-				blur: function(e){$(e.target).val(opts.currentPage);}},{currentPage:opts.currentPage, totalPages:opts.totalPages});
-			
-			return $shortPaging;
+			return base.$el.append($longPaging);
 		};
 		
-		jumpToPage = function(e){
+		base.jumpToPage = function(e){
 			var charCode = (e.which) ? e.which : e.keyCode;
-			
+
 			if (charCode > 31 && (charCode < 48 || charCode > 57)){
 				return false;
 			}
-			
+
 			if (charCode == 13){
 				e.data.page = $.trim($(e.target).val());
 				if (e.data.page != e.data.currentPage && e.data.page <= e.data.totalPages){
@@ -182,20 +202,40 @@
 					if (e.data.currentPage != e.data.page) alert("Page number is not valid");	
 				}
 			}
-			
+
 			return true;
 		};
+		
+		
+		// Run initializer
+		base.init();
+	};
 
-		return this.each(function() {
-			var $this = $(this);
-			$this.empty();
+	$.paginate.defaultOptions = {
+			type: "long",
+			currentPage:1,
+			pageSize: 10, 
+			totalItem: 10,
+			totalPages: 0,
+			pageStyle: "style1",
+			callbackText: function(itemStart, itemEnd, itemTotal){
+				return 'Displaying ' + itemStart + ' to ' + itemEnd + ' of ' + itemTotal + " Items";
+			},
+			pageLinkCallback: function(e){}, 
+			firstLinkCallback: function(e){}, 
+			nextLinkCallback: function(e){}, 
+			prevLinkCallback: function(e){},
+			lastLinkCallback: function(e){},
+			pageChangeCallback: function(e){}
+	};
 
-			var itemStart = (((opts.currentPage-1)*opts.pageSize)+1);
-			var itemEnd = (((opts.currentPage*opts.pageSize) > opts.totalItem) ? opts.totalItem : (opts.currentPage*opts.pageSize));
-			opts.totalPages = Math.ceil(opts.totalItem/opts.pageSize);
-			
-			if ($.trim(opts.type).toLowerCase()=="long") $this.append(getLongPaging(itemStart, itemEnd));
-			if ($.trim(opts.type).toLowerCase()=="short") $this.append(getShortPaging(itemStart, itemEnd));
-		});
+	$.fn.paginate = function(options){
+
+		if (this.length) {
+			return this.each(function() {
+				$(this).empty();
+				(new $.paginate(this, options));
+			});
+		};
 	};
 })(jQuery);
