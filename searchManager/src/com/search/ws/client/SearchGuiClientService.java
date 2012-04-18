9aa730856a4c2cc0acd243e760f88057cb73b127
HarrisM <HarrisM@pcmall.com>
2012-04-18 10:54:48 +0800

    delete

--- searchManager/src/com/search/ws/client/SearchGuiClientService.java
+++
@@ -1,6 +0,0 @@
-package com.search.ws.client;
-
-public interface SearchGuiClientService {
-	public boolean loadElevateList(String store, String token);
-	public boolean loadExcludeList(String store, String token);
-}
