07b0d97175f4bebf86936b1117e8232c70f29bdb
mpanalig <mpanalig@pcmall.com>
2012-04-28 02:57:10 +0800

    updated deployment pages

--- searchManager/WebContent/js/settings/approval.js
+++ searchManager/WebContent/js/settings/approval.js
@@ -118,7 +118,8 @@
 							
 							$tr.find("td#select > input[type='checkbox']").attr("id", list[i]["ruleRefId"]);
 							$tr.find("td#select > input[type='checkbox']").attr("name", list[i]["ruleStatusId"]);
-							$tr.find("td#ruleRefId > p#ruleName").html(list[i]["ruleRefId"]).on({click:previewRow},{ruleStatus:list[i]});
+							$tr.find("td#ruleOption > img.previewIcon").attr("id", list[i]["ruleRefId"]).on({click:previewRow},{ruleStatus:list[i]});
+							$tr.find("td#ruleRefId > p#ruleName").html(list[i]["ruleRefId"]);
 							if(showId) $tr.find("td#ruleRefId > p#ruleId").html(list[i]["description"]);
 							$tr.find("td#type").html(list[i]["updateStatus"]);
 							$tr.find("td#requested > p#requestedBy").html(list[i]["lastModifiedBy"]);
