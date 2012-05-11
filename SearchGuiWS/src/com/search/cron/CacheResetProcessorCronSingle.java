cf68216d97054da975b4ea380bc416c5d055e098
HarrisM <HarrisM@pcmall.com>
2012-05-11 11:36:55 +0800

    EhCache integration : Search Manager

---
+++ SearchGuiWS/src/com/search/cron/CacheResetProcessorCronSingle.java
@@ -0,0 +1,22 @@
+package com.search.cron;
+
+import org.apache.log4j.Logger;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import com.search.manager.cache.ehcache.CacheClient;
+
+public class CacheResetProcessorCronSingle extends QuartzJobBean{
+
+	private static final Logger logger = Logger.getLogger(CacheResetProcessorCronSingle.class);
+	
+	protected void executeInternal(JobExecutionContext ctx)
+			throws JobExecutionException {
+		try {
+			CacheClient cacheClient = CacheClient.getInstance();
+			cacheClient.resetAllNodeStatus();	
+		}catch (Exception e) {
+			logger.error(e,e);
+		}
+	}
+}
