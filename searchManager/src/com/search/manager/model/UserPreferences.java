2013157367fe562609efbcd134be2bce5c580a5b
eceralde <edward.ceralde@pcmall.com>
2012-05-24 11:28:36 +0800

    user preferences bean

---
+++ searchManager/src/com/search/manager/model/UserPreferences.java
@@ -0,0 +1,22 @@
+package com.search.manager.model;
+
+public class UserPreferences {
+	private String server;
+	private String store;
+	
+	public void setServer(String server) {
+		this.server = server;
+	}
+	
+	public String getServer() {
+		return server;
+	}
+	
+	public void setStore(String store) {
+		this.store = store;
+	}
+	
+	public String getStore() {
+		return store;
+	}
+}
