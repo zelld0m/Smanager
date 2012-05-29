47ab3403bd3d94deda5233ff08df59b854312040
mpanalig <mpanalig@pcmall.com>
2012-05-29 18:21:22 +0800

    cookie update

--- searchManager/src/com/search/manager/cookie/CookieUtils.java
+++ searchManager/src/com/search/manager/cookie/CookieUtils.java
@@ -7,8 +7,10 @@ import org.apache.commons.lang.StringUtils;
 public class CookieUtils {
 
 	public static Cookie expireNow(String cookieName, String path){
-		Cookie cookie = new Cookie(cookieName, "");
-		cookie.setMaxAge(-1);
+		Cookie cookie = new Cookie(cookieName, null);
+		cookie.setValue("");
+		cookie.setSecure(true);
+		cookie.setMaxAge(0);
 		if (StringUtils.isNotBlank(path)) cookie.setPath(path);
 		cookie.setComment("EXPIRING COOKIE at " + System.currentTimeMillis());
 		
