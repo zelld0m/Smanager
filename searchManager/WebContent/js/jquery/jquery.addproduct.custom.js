147d8812607fce8d941b43184ebe2ecc68b6f5f1
mpanalig <mpanalig@pcmall.com>
2012-08-23 12:17:39 +0800

    partial css update

--- searchManager/WebContent/js/jquery/jquery.addproduct.custom.js
+++ searchManager/WebContent/js/jquery/jquery.addproduct.custom.js
@@ -38,60 +38,216 @@
 
 		base.getAddProductItemTemplate = function(){
 			var template ='';
-			
-			template  += '<div>';
-			template  += '<div id="addItemTemplate" class="mar0 w250">';
-			template  += '	<h3 class="padT10"></h3>';
-			template  += '	<div class="clearB"></div>';
-			template  += '	<div class="floatL marT5 marL5">';
-			template  += '		<label class="w60 floatL padT5">SKU #: </label>';
-			template  += '		<label>';
-			template  += '			<span class="fsize10 fgray txtAR">(separated by whitespaces or commas)</span>';
-			template  += '			<textarea id="addItemDPNo" style="width: 230px; float: left; margin-bottom: 7px"></textarea>';
-			template  += '		</label>';
-			template  += '	</div>';
-			template  += '	<div class="floatL w155 marT5">';
-			template  += '		<label class="floatL w60 marL5 padT5">Valid Until:</label> ';
-			template  += '		<label class="ddate"><input id="addItemDate" type="text" class="w65"></label>';
-			template  += '	</div>';
 
-			template  += '	<div class="floatL marT5" style="width: 97px">';
-			template  += '		<label class="floatL marL5 padT5 w60">Elevation:</label>';
-			template  += '		<label><input id="addItemPosition" type="text" class="w25"></label>';
+			template  += '<div>';
+			template  += '	<div id="addItemTemplate" class="mar0 w250">';
+			template  += '		<h3 class="padT10"></h3>';
+			template  += '		<div class="clearB"></div>';
+			template  += '		<div class="floatL marT5 marL5">';
+			template  += '			<label class="w60 floatL padT5">SKU #: </label>';
+			template  += '			<label>';
+			template  += '				<span class="fsize10 fgray txtAR">(separated by whitespaces or commas)</span>';
+			template  += '				<textarea id="addItemDPNo" style="width: 230px; float: left; margin-bottom: 7px"></textarea>';
+			template  += '			</label>';
+			template  += '		</div>';
+			template  += '		<div class="floatL w155 marT5">';
+			template  += '			<label class="floatL w60 marL5 padT5">Valid Until:</label> ';
+			template  += '			<label class="ddate"><input id="addItemDate" type="text" class="w65"></label>';
+			template  += '		</div>';
+
+			template  += '		<div class="floatL marT5" style="width: 97px">';
+			template  += '			<label class="floatL marL5 padT5 w60">Elevation:</label>';
+			template  += '			<label><input id="addItemPosition" type="text" class="w25"></label>';
+			template  += '		</div>';
+			template  += '		<div class="clearB"></div>';
+			template  += '		<div class="floatL marT5 marL5">';
+			template  += '			<label class="w60 floatL padT5">Comment: </label> ';
+			template  += '			<label><textarea id="addItemComment" style="width: 230px; float: left; margin-bottom: 7px"></textarea></label>';
+			template  += '		</div>';
+			template  += '		<div align="right">';
+			template  += '			<a id="addItemToRuleBtn" href="javascript:void(0);" class="buttons btnGray clearfix">';
+			template  += '				<div class="buttons fontBold">Elevate</div>';
+			template  += '			</a>';
+			template  += '			<a id="clearBtn" href="javascript:void(0);" class="buttons btnGray clearfix">';
+			template  += '				<div class="buttons fontBold">Clear</div>';
+			template  += '			</a>';
+			template  += '		</div>';
+			template  += '		<div class="clearB"></div>';
 			template  += '	</div>';
-			template  += '	<div class="clearB"></div>';
-			template  += '	<div class="floatL marT5 marL5">';
-			template  += '		<label class="w60 floatL padT5">Comment: </label> ';
-			template  += '		<label><textarea id="addItemComment" style="width: 230px; float: left; margin-bottom: 7px"></textarea></label>';
-			template  += '	</div>';
-			template  += '	<div align="right">';
-			template  += '		<a id="addItemToRuleBtn" href="javascript:void(0);" class="buttons btnGray clearfix">';
-			template  += '			<div class="buttons fontBold">Elevate</div>';
-			template  += '		</a>';
-			template  += '		<a id="clearBtn" href="javascript:void(0);" class="buttons btnGray clearfix">';
-			template  += '			<div class="buttons fontBold">Clear</div>';
-			template  += '		</a>';
-			template  += '	</div>';
-			template  += '	<div class="clearB"></div>';
 			template  += '</div>';
-			template  += '</div>';
-			
 
 			return template;
 		},
 
-		base.promptAddProductItem = function(target){
-			$(target).qtip({
+		base.getAddFacetItemTemplate = function(){
+			var template ='';
+
+			template  += '<div id="facetItem">';
+			template  += '	<ul>';
+			template  += '		<li><a href="#ims"><span>IMS Categories/Manufacturer</span></a></li>';
+			template  += '		<li><a href="#cnet"><span>Facet Template/Manufacturer</span></a></li>';
+			template  += '		<li><a href="#dynamicAttribute"><span>Dynamic Attributes</span></a></li>';
+			template  += '		<li><a href="#facet"><span>Facets</span></a></li>';
+			template  += '	</ul>';
+			
+			template  += '	<div id="ims">';
+			template  += '	</div>';
+			
+			template  += '	<div id="cnet">';
+			template  += '		<div class="fsize12 padT40">';
+			template  += '			lorem ipsum dolor sit amet';
+			template  += '		</div>';
+			template  += '	</div>';
+			
+			template  += '	<div id="dynamicAttribute">';
+			template  += '	</div>';
+
+			template  += '	<div id="facet">';
+			template  += '		<div class="fsize12 padT40">';
+			template  += '			<table>';
+			template  += '				<tr>';
+			template  += '					<td class="w175">Name (contains) :</td>';
+			template  += '					<td class="iepadBT0">';
+			template  += '						<input id="nameContains" type="text" class="w250"/>';
+			template  += '					</td>';
+			template  += '				</tr>';
+			template  += '				<tr>';
+			template  += '					<td class="w175">Description (contains):</td>';
+			template  += '					<td class="iepadBT0">';
+			template  += '						<input id="descriptionContains" type="text" class="w250"/>';
+			template  += '					</td>';
+			template  += '				</tr>';		
+			template  += '				<tr>';
+			template  += '					<td class="w175">Platform :</td>';
+			template  += '					<td class="iepadBT0">';
+			template  += '						<select name="select" id="platformList" class="selectCombo w235" title="Select Platform" >';
+			template  += '							<option value="all"></option>';
+			template  += '							<option value="universal">Universal</option>';
+			template  += '							<option value="pc">PC</option>';
+			template  += '							<option value="linux">Linux</option>';
+			template  += '							<option value="mac">Macintosh</option>';
+			template  += '						</select>';
+			template  += '					</td>';
+			template  += '				</tr>';
+			template  += '				<tr>';
+			template  += '					<td class="w175">Condition :</td>';
+			template  += '					<td class="iepadBT0">';
+			template  += '						<select name="select" id="conditionList" class="selectCombo w235" title="Select Condition" >';
+			template  += '							<option value="all"></option>';
+			template  += '							<option value="refurbished">Refurbished</option>';
+			template  += '							<option value="open">Open Box</option>';
+			template  += '							<option value="clearance">Clearance</option>';
+			template  += '						</select>';
+			template  += '					</td>';
+			template  += '				</tr>';
+			template  += '				<tr>';
+			template  += '					<td class="w175">Availability :</td>';
+			template  += '					<td class="iepadBT0">';
+			template  += '						<select name="select" id="availabilityList" class="selectCombo w235" title="Select Availability" >';
+			template  += '							<option value="all"></option>';
+			template  += '							<option value="instock">In Stock</option>';
+			template  += '							<option value="call">Call</option>';
+			template  += '						</select>';
+			template  += '					</td>';
+			template  += '				</tr>';
+			template  += '				<tr>';
+			template  += '					<td class="w175">License :</td>';
+			template  += '					<td class="iepadBT0">';
+			template  += '						<select name="select" id="licenseList" class="selectCombo w235" title="Select License" >';
+			template  += '							<option value="all"></option>';
+			template  += '							<option value="license">Show License Products Only</option>';
+			template  += '							<option value="nonlicense">Show Non-License Products Only</option>';
+			template  += '						</select>';
+			template  += '					</td>';
+			template  += '				</tr>';
+			template  += '			</table>';
+			template  += '			<div style="clear:both">';	
+			template  += '		</div>';
+			template  += '	</div>';
+			
+			template  += '</div>';
+
+			return template;
+		};
+
+		base.promptAddFacetItem = function(api, contentHolder){
+			contentHolder.html(base.getAddFacetItemTemplate());
+
+			contentHolder.find("#facetItem").tabs({
+
+			});
+
+		};
+
+		base.promptAddProductItem = function(api, contentHolder){
+			contentHolder.html(base.getAddProductItemTemplate());
+
+			contentHolder.find("#addItemDate").attr('id', 'addItemDate_1');
+
+			contentHolder.find("#addItemDate_1").datepicker({
+				showOn: "both",
+				minDate: base.options.dateMinDate,
+				maxDate: base.options.dateMaxDate,
+				buttonText: "Expiration Date",
+				buttonImage: "../images/icon_calendar.png",
+				buttonImageOnly: true
+			});
+
+			contentHolder.find("#clearBtn").on({
+				click: function(evt){
+					contentHolder.find("input,textarea").val("");
+				}
+			});
+
+			contentHolder.find("#addItemToRuleBtn").on({
+				click: function(evt){
+
+					var commaDelimitedNumberPattern = /^\s*\d+\s*(,?\s*\d+\s*)*$/;
+
+					var skus = $.trim(contentHolder.find("#addItemDPNo").val());
+					var sequence = $.trim(contentHolder.find("#addItemPosition").val());
+					var expDate = $.trim(contentHolder.find("#addItemDate_1").val());
+					var comment = $.trim(contentHolder.find("#addItemComment").val().replace(/\n\r?/g, '<br />'));
+					var today = new Date();
+
+					today.setHours(0,0,0,0); //ignore time of current date 
+
+					if ($.isBlank(skus)) {
+						alert("There are no SKUs specified in the list.");
+					}
+					else if (!commaDelimitedNumberPattern.test(skus)) {
+						alert("List contains an invalid SKU.");
+					}							
+					else if (!$.isBlank(expDate) && !$.isDate(expDate)){
+						alert("Invalid date specified.");
+					}
+					else if(today.getTime() > new Date(expDate).getTime())
+						alert("Start date cannot be earlier than today");
+					else if (!isXSSSafe(comment)){
+						alert("Invalid comment. HTML/XSS is not allowed.");
+					}
+					else {
+						api.destroy();
+						base.options.addProductItemCallback(skus.split(/[\s,]+/), expDate, sequence, comment);						
+					}
+				}
+			});
+		};
+
+		base.promptRuleItemDetails = function(target, type){
+
+			$(target).qtip("destroy").qtip({
 				content: {
 					text: $('<div/>'),
-					title: { text: 'New Product Item', button: true
-					}
+					title: { text: type==="product"? 'Product Item' : 'Facet Item', button: true }
 				},
 				position: {
 					my: 'center',
 					at: 'center',
+					target: $(window)
 				},
 				show:{
+					ready: true,
 					modal:true
 				},
 				style: {
@@ -100,68 +256,14 @@
 				events: { 
 					show: function(event, api){
 						var contentHolder = $("div", api.elements.content);
-						contentHolder.html(base.getAddProductItemTemplate());
-						contentHolder.find("#addOption").tabs({
 
-						});
+						switch(type){
+						case "product": base.promptAddProductItem(api, contentHolder); break; 
+						case "ims": base.promptAddFacetItem(api, contentHolder); break;
+						case "cnet": base.promptAddFacetItem(api, contentHolder); break;
+						case "facet": base.promptAddFacetItem(api, contentHolder); break;
+						};
 
-						contentHolder.find("#addItemDate").attr('id', 'addItemDate_1');
-
-						contentHolder.find("#addItemDate_1").datepicker({
-							showOn: "both",
-							minDate: base.options.dateMinDate,
-							maxDate: base.options.dateMaxDate,
-							buttonText: "Expiration Date",
-							buttonImage: "../images/icon_calendar.png",
-							buttonImageOnly: true
-						});
-
-						contentHolder.find("#clearBtn").on({
-							click: function(evt){
-								contentHolder.find("input,textarea").val("");
-							}
-						});
-
-						contentHolder.find("#addItemToRuleBtn").on({
-							click: function(evt){
-
-								var commaDelimitedNumberPattern = /^\s*\d+\s*(,?\s*\d+\s*)*$/;
-
-								var skus = $.trim(contentHolder.find("#addItemDPNo").val());
-								var sequence = $.trim(contentHolder.find("#addItemPosition").val());
-								var expDate = $.trim(contentHolder.find("#addItemDate_1").val());
-								var comment = $.trim(contentHolder.find("#addItemComment").val().replace(/\n\r?/g, '<br />'));
-								var today = new Date();
-								//ignore time of current date 
-								today.setHours(0,0,0,0);
-
-								if ($.isBlank(skus)) {
-									alert("There are no SKUs specified in the list.");
-								}
-								else if (!commaDelimitedNumberPattern.test(skus)) {
-									alert("List contains an invalid SKU.");
-								}							
-								else if (!$.isBlank(expDate) && !$.isDate(expDate)){
-									alert("Invalid date specified.");
-								}
-								else if(today.getTime() > new Date(expDate).getTime())
-									alert("Start date cannot be earlier than today");
-								else if (!isXSSSafe(comment)){
-									alert("Invalid comment. HTML/XSS is not allowed.");
-								}
-								else {								
-									ElevateServiceJS.addItemToRuleUsingPartNumber(selectedRule.ruleId, sequence, expDate, comment, skus.split(/[\s,]+/), {
-										callback : function(code){
-											showActionResponseFromMap(code, "add", skus, "Please check for the following:\n a) SKU(s) are already present in the list\n b) SKU(s) are actually searchable using the specified keyword.");
-											showElevate();
-										},
-										preHook: function(){ 
-											prepareElevate();
-										}
-									});								
-								}
-							}
-						});
 					},
 					hide: function(event, api){
 						api.destroy();
@@ -173,14 +275,9 @@
 		base.addButtonListener = function(){
 			base.$el.find('a#addRuleItemIcon').off().on({
 				click: function(e){
-					switch($.trim(base.$el.find('select#selectRuleItemType').val()).toLowerCase()){
-					case "product": base.promptAddProductItem(this); break; 
-					case "ims": break; 
-					case "cnet": break; 
-					case "facet": break; 
-					};
+					base.promptRuleItemDetails(this, $.trim(base.$el.find('select#selectRuleItemType').val()).toLowerCase());
 				}
-			}, {locked: base.options.locked});
+			});
 		};
 
 		// Run initializer
@@ -191,7 +288,8 @@
 			locked: true,
 			dateMinDate: 0,
 			dateMaxDate: "+1Y",
-
+			addProductItemCallback: function(skus, expDate, sequence, comment){},
+			addFacetItemCallback: function(){}
 	};
 
 	$.fn.addproduct = function(options){
