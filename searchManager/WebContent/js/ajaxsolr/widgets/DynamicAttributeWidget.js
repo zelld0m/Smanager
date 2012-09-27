68dcc6af9b707ab4b29c54c31e897310ebfcbf02
iflores <irish.flores@pcmall.com>
2012-09-27 20:02:28 +0800

    Bug #13597 [PM - SCHMGR03] CRF47171: Exclude: Items excluded under dynamic attributes rule condition are displayed in search result.

--- searchManager/WebContent/js/ajaxsolr/widgets/DynamicAttributeWidget.js
+++ searchManager/WebContent/js/ajaxsolr/widgets/DynamicAttributeWidget.js
@@ -103,6 +103,13 @@
 						'json.nl':'map'
 				};
 
+				var storeparams = self.manager.store.params;
+				//merge params
+				for(var name in storeparams){
+					if(params[storeparams[name].name] === undefined)
+						params[storeparams[name].name] = storeparams[name].value;
+				}
+				
 				for (var name in params) {
 					if ($.isArray(params[name])){
 						for (var param in params[name]){
