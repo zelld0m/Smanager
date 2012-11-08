d0806ebf7879079aa9de2e793a4c0178d29509ab
eceralde <edward.ceralde@pcmall.com>
2012-11-08 18:32:17 +0800

    ExportType

---
+++ searchManager/src/com/search/manager/enums/ExportType.java
@@ -0,0 +1,59 @@
+package com.search.manager.enums;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.directwebremoting.annotations.DataTransferObject;
+import org.directwebremoting.convert.EnumConverter;
+
+@DataTransferObject(converter=EnumConverter.class)
+public enum ExportType {
+
+	AUTOMATIC("Automatic", "Automatic"),
+	MANUAL("Manual", "Manual");
+
+	private final String displayText;
+	private final String description;
+	
+	private static final Map<String,SortType> lookup 
+    = new HashMap<String,SortType>();
+	
+	ExportType(String displayText, String description){
+		this.displayText = displayText;
+		this.description = description;
+	}
+	
+	static {
+         for(SortType s : EnumSet.allOf(SortType.class))
+              lookup.put(s.getDisplayText(), s);
+    }
+	
+	public String getDisplayText() {
+		return displayText;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	public List<SortType> getSortOrder(){
+		return Arrays.asList(SortType.values());
+	}
+	
+	public static SortType get(String displayText) { 
+         return lookup.get(displayText); 
+    }
+	
+	public static ExportType get(Integer intValue) { 
+        return ExportType.values()[intValue-1];
+    }
+	
+	@Override
+	public String toString() {
+		return String.valueOf(ordinal()+1);
+	}
+	
+}
