09ba34bb13f49dc3e8e89fa80c97a957de4c92c8
mpanalig <mpanalig@pcmall.com>
2013-01-25 11:39:14 +0800

    Fixes related to Bug#15237 conversion of special chars to html codes

--- searchManager/WebContent/js/jquery/ajaxsolr.custom/browse.theme.js
+++ searchManager/WebContent/js/jquery/ajaxsolr.custom/browse.theme.js
@@ -303,9 +303,9 @@
 		secObj.find("div#cartPriceHolder").append('$' + doc[GLOBAL_storeFacetName + "_CartPrice"]);
 
 		var name = $.isNotBlank(doc[GLOBAL_storeFacetName + "_Name"])? doc[GLOBAL_storeFacetName + "_Name"] : doc.Name;
-		var manufacturer = '<span class="txtManufact">' + doc.Manufacturer + '</span>';
+		var manufacturer = '<span class="txtManufact fbold">' + doc.Manufacturer + '</span> ';
 		
-		secObj.find("div#docHolder").wrapInner(AjaxSolr.theme('createLink', manufacturer + name, docHandler));
+		secObj.find("div#docHolder").wrapInner(AjaxSolr.theme('createLink', name, docHandler)).prepend(manufacturer);
 
 		//Add Audit Button
 		secObj.find("div#auditHolder").html(AjaxSolr.theme('createLink', '', auditHandler));
