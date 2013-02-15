735aa297951751c0c649fae28880d1efc870b922
mpanalig <mpanalig@pcmall.com>
2013-02-15 10:48:37 +0800

    reorganize contents

--- searchManager/WebContent/js/jquery/ajaxsolr.custom/widgets/ActiveRuleWidget.js
+++ searchManager/WebContent/js/jquery/ajaxsolr.custom/widgets/ActiveRuleWidget.js
@@ -1,6 +1,12 @@
 (function ($) {
 	
 	AjaxSolr.ActiveRuleWidget = AjaxSolr.AbstractWidget.extend({
+		init: function(){
+			var settings = $.cookie('ar.' + GLOBAL_username);
+			if($.isBlank(settings)){
+				$.cookie('ar.' + GLOBAL_username, "collapse" ,{path:GLOBAL_contextPath});
+			}
+		},
 		
 		getRuleStatus:function($li, rule){
 			DeploymentServiceJS.getRuleStatus(rule["type"], rule["id"], {
@@ -8,7 +14,7 @@
 					if(!$.isEmptyObject(data)){
 						$li.find('.ruleStatus > .status').text(getRuleNameSubTextStatus(data));
 						$li.find('.ruleStatus > .statusMode').text($.isNotBlank(data["locked"]) && data["locked"]? " [ Read-Only ]" : "");
-						$li.find('.lastPublished').text($.isNotBlank(data["lastPublishedDate"])? 'Last Published: ' + data["lastPublishedDate"].toUTCString(): '');
+						$li.find('.lastPublished').text($.isNotBlank(data["lastPublishedDate"])? 'Published on ' + data["lastPublishedDate"].toUTCString(): '');
 					}
 				}
 			});
@@ -16,63 +22,29 @@
 		
 		beforeRequest:function(){
 			var self = this;
+	
+			$.cookie('ar.' + GLOBAL_username, "expand" ,{path:GLOBAL_contextPath});
+			$(self.target).find('#switcher').off();
+			$(self.target).find('#switcherText').text("Active Rule");
 			
+			//TODO: Remove/Update
 			$(self.target).find('ul#itemListing > li.items:not(#itemPattern)').each(function(idx, el){
 				$(el).find('.select > input[type="checkbox"]').prop("disabled", true);
 				$(el).find('.preloader').show();
 			});
 		},
 		
-		getTemplate: function(){
-			var output  = '';
-
-			output  +='<div style="display:block;" class="fsize11 marT10 fDGray border">';
-			output  +='	<div id="activeRuleNote" class="w655 marL20 info notification border fsize11 marB20 marT10">';
-			output  +=' 	Below are rules applied to your current search. You can toggle ON/OFF of each active rules to examine its effect on search results';
-			output  +=' </div>';
-			output  +='	<ul id="itemListing" class="mar16 marB20 marL20" >';
-			output  +='		<li id="itemPattern" class="items borderB padTB5 clearfix" style="display:none; width:690px">';
-			output  +='			<div class="floatL"><label class="ruleType fbold w160"></label></div>';
-			output	+='			<div class="floatL">';
-			output  +='				<div class="w230"><label class="imageIcon w20 floatL posRel topn2"><img src="' + AjaxSolr.theme('getAbsoluteLoc', "images/icon_reviewContent2.png")  + '" class="top2 posRel"></label>';
-			output  +='				<label class="name floatL w210"><span class="fbold"></span></label></div><div class="clearB"></div>';
-			output  +='				<div class="w230"><label class="ruleStatus w230 marT6">';
-			output  +='					<span class="status fgray"></span>';
-			output  +='					<span class="statusMode fsize11 forange padL5"></span>';
-			output  +='				</label></div>';
-			output	+='			</div>';
-			output	+='			<div class="floatR w300">';
-			output  +='				<div class="w300">';
-			output  +='					<label class="select floatR w20 posRel topn3"><input type="checkbox" class="firerift-style-checkbox on-off ruleControl"></label>';
-			output  +='					<label class="w30 preloader floatR" style="display:none"><img src="' + AjaxSolr.theme('getAbsoluteLoc', "images/ajax-loader-rect.gif")  + '"></label>';
-			output  +='				</div>';
-			output  +='				<div class="w300 floatR"><label class="lastPublished fgray"></label></div>';
-			output	+='			</div>';
-			output  +='		</li>';
-			output  +='	</ul>';
-			output  +='<div class="clearB"></div>';
-			output  +='</div>';
-			output  +='<a href="javascript:void(0);">';
-			output  +='<div class="minW100 floatR borderB borderR borderL height23 posRel topn1 fbold fsize11 padT8 marL5" style="display:block; background: #fff; z-index:500; color:#329eea;">';
-			output  +='	<img src="' + AjaxSolr.theme('getAbsoluteLoc', "images/icon_arrowDownBlue.png")  + '" class="top2 posRel marL5 marR3">';
-			output  +='	<span>Active Rules</span>';
-			output  +='</div>';
-			output  +='</a>';
-
-			return $(output);
-		},
-		
 		afterRequest: function () {
 			var self = this;
 			$(self.target).empty();
 			var keyword = $.isArray(self.manager.store.values('q'))? self.manager.store.values('q')[0]: self.manager.store.values('q');
-			
 			if($.isNotBlank(keyword)){
 				$(self.target).html(self.getTemplate());
 				
 				var rules = self.manager.response.responseHeader["search_rules"];
 				var $ul = $(self.target).find("ul#itemListing");
 				$ul.find("li.items:not(#itemPattern)").remove();
+				$(self.target).find('#switcherText').text(rules.length + ' Active ' + (rules.length > 1 ? 'Rules': 'Rule'))
 				
 				for(var i=0; i<rules.length; i++){
 					var rule = rules[i]["rule"];
@@ -150,9 +122,98 @@
 				
 				$ul.find("li").removeClass("alt");
 				$ul.find("li:even").addClass("alt");
+
+				if($.cookie('ar.' + GLOBAL_username) === "expand"){
+					self.showRules();
+				}else{
+					self.hideRules();
+				}
 				
+				$(self.target).find('#switcher').off().on({
+					click: function(e){
+						if ($(self.target).find("#collapse").is(':visible')){
+							$.cookie('ar.' + GLOBAL_username, "expand" ,{path:GLOBAL_contextPath});
+							self.showRules();
+						}else{
+							$.cookie('ar.' + GLOBAL_username, "collapse" ,{path:GLOBAL_contextPath});
+							self.hideRules();
+						}
+					}
+				});
 			}
+		},
+		
+		showRules: function(){
+			var self = this;
+			$(self.target).find("#collapse").fadeOut("slow", function(foe){
+				$(self.target).find("#expand").slideDown("slow", function(sde){
+					
+				});
+			});
+		},
+		
+		hideRules: function(){
+			var self = this;
+			
+			$(self.target).find("#expand").fadeOut("slow", function(foe){
+				$(self.target).find("#collapse").slideDown("slow", function(sde){
+					
+				});
+			});
+		},
+		
+		getTemplate: function(){
+			var output  = '';
+
+			output  +='<div style="display:block;" class="fsize11 marT10 fDGray border">';
+			output  +='	<div id="expand" style="display:none">';
+			output  +='		<div id="activeRuleNoteHide" class="w655 marL20 info notification border fsize11 marB20 marT10">';
+			output  +=' 		Below are rules applied to your current search. You can toggle ON/OFF of each active rules to examine its effect on search results';
+			output  +=' 	</div>';
+			output  +='		<ul id="itemListing" class="mar16 marB20 marL20" >';
+			output  +='			<li id="itemPattern" class="items borderB padTB5 clearfix" style="display:none; width:690px">';
+			output  +='				<div class="floatL marT6">';
+			output  +='					<label class="select floatL w80 posRel topn3"><input type="checkbox" class="firerift-style-checkbox on-off ruleControl"></label>';
+			output  +='				</div>';
+			output	+='				<div class="floatR w300">';
+			output  +='					<div class="w300">';
+			output  +='						<label class="w30 preloader" style="display:none"><img src="' + AjaxSolr.theme('getAbsoluteLoc', "images/ajax-loader-rect.gif")  + '"></label>';
+			output  +='						<label class="ruleStatus w230 marT6">';
+			output  +='							<span class="status fgray"></span>';
+			output  +='							<span class="statusMode fsize11 forange padL5"></span>';
+			output  +='						</label>';
+			output  +='					</div>';
+			output  +='					<div class="w300 floatR"><label class="lastPublished fgray"></label></div>';
+			output	+='				</div>';
+			output	+='				<div class="floatR">';
+			output  +='					<div class="w230">';
+			output  +='						<label class="ruleType fbold w230 marT6"></label>';
+			output  +='					</div>';
+			output  +='					<div class="w230">';
+			output  +='						<label class="imageIcon w20 floatL posRel topn2"><img src="' + AjaxSolr.theme('getAbsoluteLoc', "images/icon_reviewContent2.png")  + '" class="top2 posRel"></label>';
+			output  +='						<label class="name floatL w210"><span class="fbold"></span></label>';
+			output  +='					</div><div class="clearB"></div>';
+			output	+='				</div>';
+			output  +='			</li>';
+			output  +='		</ul>';
+			output  +='	</div>';
+			output  +='	<div id="collapse" style="display:none">';
+			output  +='		<div id="activeRuleNoteShow" class="w655 marL20 info notification border fsize11 marB20 marT10">';
+			output  +=' 		<label>Toggle this section to view all rules applied to this rule</label>';
+			output  +=' 	</div>';
+			output  +='	</div>';
+			output  +='</div>';
+			
+			output  +='<a id="switcher" href="javascript:void(0);">';
+			output  +='	<div class="minW100 floatR borderB borderR borderL height23 posRel topn1 fbold fsize11 padT8 marL5" style="display:block; background: #fff; z-index:500; color:#329eea;">';
+			output  +='		<img src="' + AjaxSolr.theme('getAbsoluteLoc', "images/icon_arrowDownBlue.png")  + '" class="top2 posRel marL5 marR3">';
+			output  +='		<span id="switcherText"></span>';
+			output  +='	</div>';
+			output  +='</a>';
+
+			return $(output);
 		}
+		
 	});
 
 })(jQuery);
