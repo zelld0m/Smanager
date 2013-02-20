892c84bc13563ecc31f9491b8b652f77a346e499
jjamolod <jesrome.jamolod@pcmall.com>
2013-02-20 13:30:00 +0800

    - Formatted.

--- SearchGuiWS/src/com/search/webservice/SearchGuiServiceImpl.java
+++ SearchGuiWS/src/com/search/webservice/SearchGuiServiceImpl.java
@@ -11,118 +11,132 @@ import com.search.manager.utility.PropsUtils;
 import com.search.service.DeploymentRuleService;
 import com.search.webservice.model.TransportList;
 
-public class SearchGuiServiceImpl implements SearchGuiService{
+public class SearchGuiServiceImpl implements SearchGuiService {
 
 	private static Logger logger = Logger.getLogger(SearchGuiServiceImpl.class);
 	private static String token;
 	private static final String RESOURCE_MAP = "token";
-	
+
 	private static DeploymentRuleService deploymentRuleService;
-	
-	public void setDeploymentRuleService(DeploymentRuleService deploymentRuleService_) {
+
+	public void setDeploymentRuleService(
+			DeploymentRuleService deploymentRuleService_) {
 		deploymentRuleService = deploymentRuleService_;
 	}
-	
-	static{
+
+	static {
 		try {
 			token = PropsUtils.getValue(RESOURCE_MAP);
 		} catch (Exception ex) {
 			ex.printStackTrace(System.err);
 		}
 	}
-	
+
 	@Override
-	public Map<String,Boolean> deployRulesMap(TransportList list) {
+	public Map<String, Boolean> deployRulesMap(TransportList list) {
 
 		try {
-			if(isValidToken(list.getToken())){
+			if (isValidToken(list.getToken())) {
 				List<String> ruleList = list.getList();
-				
-				if(CollectionUtils.isNotEmpty(ruleList)){
-					
+
+				if (CollectionUtils.isNotEmpty(ruleList)) {
+
 					switch (list.getRuleEntity()) {
 					case ELEVATE:
-						return deploymentRuleService.publishElevateRulesMap(list.getStore(), ruleList);
+						return deploymentRuleService.publishElevateRulesMap(
+								list.getStore(), ruleList);
 					case EXCLUDE:
-						return deploymentRuleService.publishExcludeRulesMap(list.getStore(), ruleList);
+						return deploymentRuleService.publishExcludeRulesMap(
+								list.getStore(), ruleList);
 					case DEMOTE:
-						return deploymentRuleService.publishDemoteRulesMap(list.getStore(), ruleList);
+						return deploymentRuleService.publishDemoteRulesMap(
+								list.getStore(), ruleList);
 					case FACET_SORT:
-						return deploymentRuleService.publishFacetSortRulesMap(list.getStore(), ruleList);
-					case KEYWORD: 
+						return deploymentRuleService.publishFacetSortRulesMap(
+								list.getStore(), ruleList);
+					case KEYWORD:
 						break;
-					case STORE_KEYWORD: 
+					case STORE_KEYWORD:
 						break;
 					case CAMPAIGN:
 						break;
 					case BANNER:
 						break;
 					case QUERY_CLEANING:
-						return deploymentRuleService.publishRedirectRulesMap(list.getStore(), ruleList);
+						return deploymentRuleService.publishRedirectRulesMap(
+								list.getStore(), ruleList);
 					case RANKING_RULE:
-						return deploymentRuleService.publishRankingRulesMap(list.getStore(), ruleList);
+						return deploymentRuleService.publishRankingRulesMap(
+								list.getStore(), ruleList);
 					default:
 						break;
 					}
 				}
 			}
 		} catch (Exception e) {
-			logger.error(e,e);
+			logger.error(e, e);
 		}
-		return new HashMap<String,Boolean>();
+		return new HashMap<String, Boolean>();
 	}
 
 	@Override
-	public Map<String,Boolean> unDeployRulesMap(TransportList list) {
+	public Map<String, Boolean> unDeployRulesMap(TransportList list) {
 		try {
-			if(isValidToken(list.getToken())){
+			if (isValidToken(list.getToken())) {
 				List<String> ruleList = list.getList();
-				
-				if(CollectionUtils.isNotEmpty(ruleList)){
-					
+
+				if (CollectionUtils.isNotEmpty(ruleList)) {
+
 					switch (list.getRuleEntity()) {
 					case ELEVATE:
-						return deploymentRuleService.unpublishElevateRulesMap(list.getStore(), ruleList);
+						return deploymentRuleService.unpublishElevateRulesMap(
+								list.getStore(), ruleList);
 					case EXCLUDE:
-						return deploymentRuleService.unpublishExcludeRulesMap(list.getStore(), ruleList);
+						return deploymentRuleService.unpublishExcludeRulesMap(
+								list.getStore(), ruleList);
 					case DEMOTE:
-						return deploymentRuleService.unpublishDemoteRulesMap(list.getStore(), ruleList);
+						return deploymentRuleService.unpublishDemoteRulesMap(
+								list.getStore(), ruleList);
 					case FACET_SORT:
-						return deploymentRuleService.unpublishFacetSortRulesMap(list.getStore(), ruleList);
-					case KEYWORD: 
+						return deploymentRuleService
+								.unpublishFacetSortRulesMap(list.getStore(),
+										ruleList);
+					case KEYWORD:
 						break;
-					case STORE_KEYWORD: 
+					case STORE_KEYWORD:
 						break;
 					case CAMPAIGN:
 						break;
 					case BANNER:
 						break;
 					case QUERY_CLEANING:
-						return deploymentRuleService.unpublishRedirectRulesMap(list.getStore(), ruleList);
+						return deploymentRuleService.unpublishRedirectRulesMap(
+								list.getStore(), ruleList);
 					case RANKING_RULE:
-						return deploymentRuleService.unpublishRankingRulesMap(list.getStore(), ruleList);
+						return deploymentRuleService.unpublishRankingRulesMap(
+								list.getStore(), ruleList);
 					default:
 						break;
 					}
 				}
 			}
 		} catch (Exception e) {
-			logger.error(e,e);
+			logger.error(e, e);
 		}
-		return new HashMap<String,Boolean>();
+		return new HashMap<String, Boolean>();
 	}
-	
-	private boolean isValidToken(String token_){	
-		try{	
-			if(token.equals(token_)){
+
+	private boolean isValidToken(String token_) {
+		try {
+			if (token.equals(token_)) {
 				logger.info("User has valid token ... ");
 				return true;
-			}else{
+			} else {
 				logger.info("User has invalid token ... ");
 				return false;
 			}
-		}catch(Exception e){
-			logger.error(e,e);
+		} catch (Exception e) {
+			logger.error(e, e);
 		}
 		return false;
 	}
