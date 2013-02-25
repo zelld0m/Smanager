6886a22cacd87425b0b9b2856b57fb8a1959d8d1
iflores <Irish.Flores@pcmall.com>
2013-02-25 10:07:53 +0800

    Bug#15871:[MM - SCHMGR03] CRF47516 - [Demote]: Able to add blank comments Bug#15872:[MM - SCHMGR03] CRF47516 - [Demote]: Successful adding of invalid comments

--- searchManager/WebContent/js/jquery/plugin.custom/jquery.comment.custom.js
+++ searchManager/WebContent/js/jquery/plugin.custom/jquery.comment.custom.js
@@ -140,11 +140,16 @@
 						base.contentHolder.find("#addCommentBtn").off().on({
 							click: function(e){
 								var comment= $.defaultIfBlank($.trim(base.contentHolder.find("#comment").val()), "");
-								if(!isXSSSafe(comment)){
+								if ($.isBlank(comment)){
+									jAlert("Comment cannot be blank.","Comment");
+								}
+								else if(!isXSSSafe(comment)){
 									jAlert("Invalid comment. HTML/XSS is not allowed.","Comment");
-								}else if ($.isNotBlank(comment))
+								} 
+								else{
 									var nl2br = comment.replace(/\n\r?/g, '<br/>');
 									base.options.itemAddComment(base, nl2br);
+								}
 							}
 						});
 					},
