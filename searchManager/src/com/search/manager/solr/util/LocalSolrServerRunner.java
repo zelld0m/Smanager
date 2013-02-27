039e48093cfd8ca08c862d6311881cf38c1287ac
jjamolod <jesrome.jamolod@pcmall.com>
2013-02-27 16:31:26 +0800

    - Removed EmbeddedSolrServer. - Changed SolrServer to HttpSolrServer. - Added HttpSolrServer connection properties.

--- searchManager/src/com/search/manager/solr/util/LocalSolrServerRunner.java
+++ searchManager/src/com/search/manager/solr/util/LocalSolrServerRunner.java
@@ -1,98 +1,125 @@
 package com.search.manager.solr.util;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.client.solrj.response.SolrPingResponse;
 import org.apache.solr.client.solrj.response.UpdateResponse;
 import org.apache.solr.common.SolrInputDocument;
-import org.apache.solr.core.CoreContainer;
 
 public class LocalSolrServerRunner {
 
-	private SolrServer solrServer;
+	private HttpSolrServer httpSolrServer;
 	private String coreName;
 	private String solrUrl;
+	private int maxRetries;
+	private int connectionTimeout;
+	private int soTimeout;
+	private int defaultMaxConnectionsPerHost;
+	private int maxTotalConnections;
+	private boolean followRedirects = false;
+	private boolean allowCompression = true;
+	private Timer timer = null;
 
-	@SuppressWarnings("unused")
 	private LocalSolrServerRunner() {
 		// do nothing...
 	}
 
-	public LocalSolrServerRunner(String solrHome, File solrConfigFile)
-			throws SolrServerException {
+	public boolean initLocalSolrServerRunner() {
 		try {
-			File f = new File(solrConfigFile, "solr.xml");
-			CoreContainer coreContainer = new CoreContainer(solrHome, f);
-			solrServer = new EmbeddedSolrServer(coreContainer, coreName);
+			if (timer != null) {
+				timer.cancel();
+			}
+
+			if (!solrUrl.endsWith("/")) {
+				solrUrl += "/";
+			}
+			httpSolrServer = new HttpSolrServer(solrUrl + coreName);
+			// Default to 0. > 1 not recommended.
+			httpSolrServer.setMaxRetries(maxRetries);
+			httpSolrServer.setConnectionTimeout(connectionTimeout);
+			// This is desirable for queries, but probably not for indexing.
+			httpSolrServer.setSoTimeout(soTimeout); // socket read timeout
+			httpSolrServer
+					.setDefaultMaxConnectionsPerHost(defaultMaxConnectionsPerHost);
+			httpSolrServer.setMaxTotalConnections(maxTotalConnections);
+			// Default to false.
+			httpSolrServer.setFollowRedirects(followRedirects);
+			// allowCompression defaults to false.
+			// Server side must support gzip or deflate for this to have any
+			// effect.
+			httpSolrServer.setAllowCompression(allowCompression);
+
+			timer = new Timer(true);
+			timer.schedule(new TimerTask() {
+				@Override
+				public void run() {
+					if (httpSolrServer.getHttpClient() != null) {
+						httpSolrServer.getHttpClient().getConnectionManager()
+								.closeExpiredConnections();
+					}
+				}
+			}, 5000);
+
 		} catch (Exception e) {
-			throw new SolrServerException(e);
+			return false;
 		}
-	}
 
-	public LocalSolrServerRunner(String solrUrl, String coreName)
-			throws SolrServerException {
-		this.solrUrl = solrUrl;
-		this.coreName = coreName;
-
-		if (!solrUrl.endsWith("/")) {
-			solrUrl += "/";
-		}
-		solrServer = new HttpSolrServer(solrUrl + coreName);
+		return true;
 	}
 
 	public UpdateResponse addDocs(List<SolrInputDocument> solrDocs)
 			throws SolrServerException, IOException {
-		return solrServer.add(solrDocs);
+		return httpSolrServer.add(solrDocs);
 	}
 
 	public UpdateResponse addDoc(SolrInputDocument doc)
 			throws SolrServerException, IOException {
-		return solrServer.add(doc);
+		return httpSolrServer.add(doc);
 	}
 
 	public UpdateResponse commit() throws SolrServerException, IOException {
-		return solrServer.commit();
+		return httpSolrServer.commit();
 	}
 
 	public UpdateResponse softCommit() throws SolrServerException, IOException {
-		return solrServer.commit(false, false, true);
+		return httpSolrServer.commit(false, false, true);
 	}
 
 	public UpdateResponse rollback() throws SolrServerException, IOException {
-		return solrServer.rollback();
+		return httpSolrServer.rollback();
 	}
 
 	public UpdateResponse optimize() throws SolrServerException, IOException {
-		return solrServer.optimize();
+		return httpSolrServer.optimize();
 	}
 
 	public UpdateResponse deleteById(String id) throws SolrServerException,
 			IOException {
-		return solrServer.deleteById(id);
+		return httpSolrServer.deleteById(id);
 	}
 
 	public UpdateResponse deleteByQuery(String query)
 			throws SolrServerException, IOException {
-		return solrServer.deleteByQuery(query);
+		return httpSolrServer.deleteByQuery(query);
 	}
 
 	public QueryResponse query(SolrQuery solrQuery) throws SolrServerException,
 			IOException {
-		return solrServer.query(solrQuery);
+		return httpSolrServer.query(solrQuery);
 	}
 
 	public int ping() {
 		SolrPingResponse solrPingResponse;
 		try {
-			solrPingResponse = solrServer.ping();
+			solrPingResponse = httpSolrServer.ping();
 			return solrPingResponse.getStatus();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -102,15 +129,15 @@ public class LocalSolrServerRunner {
 	}
 
 	public void shutdown() {
-		solrServer.shutdown();
+		httpSolrServer.shutdown();
 	}
 
 	public SolrServer getSolrServer() {
-		return solrServer;
+		return httpSolrServer;
 	}
 
-	public void setSolrServer(SolrServer solrServer) {
-		this.solrServer = solrServer;
+	public void setSolrServer(HttpSolrServer solrServer) {
+		this.httpSolrServer = solrServer;
 	}
 
 	public String getCoreName() {
@@ -127,6 +154,70 @@ public class LocalSolrServerRunner {
 
 	public void setSolrUrl(String solrUrl) {
 		this.solrUrl = solrUrl;
+	}
+
+	public HttpSolrServer getHttpSolrServer() {
+		return httpSolrServer;
+	}
+
+	public void setHttpSolrServer(HttpSolrServer httpSolrServer) {
+		this.httpSolrServer = httpSolrServer;
+	}
+
+	public int getMaxRetries() {
+		return maxRetries;
+	}
+
+	public void setMaxRetries(int maxRetries) {
+		this.maxRetries = maxRetries;
+	}
+
+	public int getConnectionTimeout() {
+		return connectionTimeout;
+	}
+
+	public void setConnectionTimeout(int connectionTimeout) {
+		this.connectionTimeout = connectionTimeout;
+	}
+
+	public int getSoTimeout() {
+		return soTimeout;
+	}
+
+	public void setSoTimeout(int soTimeout) {
+		this.soTimeout = soTimeout;
+	}
+
+	public int getDefaultMaxConnectionsPerHost() {
+		return defaultMaxConnectionsPerHost;
+	}
+
+	public void setDefaultMaxConnectionsPerHost(int defaultMaxConnectionsPerHost) {
+		this.defaultMaxConnectionsPerHost = defaultMaxConnectionsPerHost;
+	}
+
+	public int getMaxTotalConnections() {
+		return maxTotalConnections;
+	}
+
+	public void setMaxTotalConnections(int maxTotalConnections) {
+		this.maxTotalConnections = maxTotalConnections;
+	}
+
+	public boolean isFollowRedirects() {
+		return followRedirects;
+	}
+
+	public void setFollowRedirects(boolean followRedirects) {
+		this.followRedirects = followRedirects;
+	}
+
+	public boolean isAllowCompression() {
+		return allowCompression;
+	}
+
+	public void setAllowCompression(boolean allowCompression) {
+		this.allowCompression = allowCompression;
 	}
 
 }
