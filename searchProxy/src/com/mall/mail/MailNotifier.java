1cccbd04144c83f8059f03ab7828f2ef65f6818b
jjamolod <jesrome.jamolod@pcmall.com>
2013-03-12 13:06:32 +0800

    - Formatted.

--- searchProxy/src/com/mall/mail/MailNotifier.java
+++ searchProxy/src/com/mall/mail/MailNotifier.java
@@ -43,7 +43,7 @@ public class MailNotifier {
 		setMessage(new MimeMessage(session));
 		getMessage().setSubject(
 				localhostname
-						+ " "
+						+ ": "
 						+ StringUtil.ifNull(subject,
 								prop.getProperty("mail.subject")));
 		getMessage().setRecipients(
