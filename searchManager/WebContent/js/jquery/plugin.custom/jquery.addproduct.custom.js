ead2255bbe2cac0902db9affd19c4ef9c20fff42
jjamolod <jesrome.jamolod@pcmall.com>
2013-03-14 11:15:43 +0800

    - Fixed for date picker is automatically displayed when alert box pops up.

--- searchManager/WebContent/js/jquery/plugin.custom/jquery.addproduct.custom.js
+++ searchManager/WebContent/js/jquery/plugin.custom/jquery.addproduct.custom.js
@@ -329,6 +329,7 @@
 			template  += '				<tr>';
 			template  += '					<td class="w175">Valid Until: </td>';
 			template  += '					<td class="iepadBT0 padT1">';
+			template  += '			            <input id="hideCursor" type="txt" style="position:absolute; top:-30px; padding:1px; margin-left:-1px; opacity:0.00; border:none; height:0px; width:0px"/>';
 			template  += '						<div class="floatL w100 marT5">';
 			template  += '							<label class="ddate"><input id="addItemDate" type="text" class="w65"></label>';
 			template  += '						</div>';
@@ -1431,9 +1432,15 @@
 			base.contentHolder.find("#facetItem").tabs("destroy").tabs({
 				show: function(event, ui){
 					if(ui.panel){
-						if (ui.panel.id === "dynamicAttribute") {
-							base.contentHolder.find("#templateNameList").focus();;
-						}
+//						if (ui.panel.id === "dynamicAttribute") {
+//							base.contentHolder.find("#templateNameList").focus();
+//						} 
+//						else if (ui.panel.id === "cnet") {
+//							base.contentHolder.find("#hideCursor").focus();
+//						}  else if (ui.panel.id === "facet") {
+//							base.contentHolder.find("#hideCursor").focus();
+//						}
+						base.contentHolder.find("#hideCursor").focus();
 					}
 				}
 			});
@@ -1452,11 +1459,11 @@
 			base.contentHolder.find("#addFacetItemToRuleBtn").off().on({
 				click: function(e){
 					setTimeout(function() {
-
+						$("#ui-datepicker-div").hide();
 						var position = 1;
 						var valid = true;
 						var today = new Date();
-						today.setHours(0,0,0,0); //ignore time of current date 
+						today.setHours(0,0,0,0); //ignore time of current date
 						
 						if (base.options.showPosition){
 							position = base.contentHolder.find("#addItemPosition").val();
@@ -1521,6 +1528,7 @@
 								base.options.updateFacetItemCallback(base.options.item["memberId"], position, expiryDate, comment, condMap);
 							}
 						}
+						$("#ui-datepicker-div").hide();
 					}, 500);
 				}
 			});
