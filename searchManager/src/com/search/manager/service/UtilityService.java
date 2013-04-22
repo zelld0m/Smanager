053c8b2f68053b74188d3317c7e317942cf3039a
eceralde <edward.ceralde@pcmall.com>
2013-04-22 14:04:50 +0800

    get user info before calling publish. This is to ensure lock can be released using the user data accessible even if user session has already expired.

--- searchManager/src/com/search/manager/service/UtilityService.java
+++ searchManager/src/com/search/manager/service/UtilityService.java
@@ -58,33 +58,34 @@ public class UtilityService {
 		}
 	}
 	
-	public static boolean obtainPublishLock(RuleEntity ruleType) throws PublishLockException {
-		String username = getUsername();
-		String storeName = getStoreName();
+	public static boolean obtainPublishLock(RuleEntity ruleType, String username, String storeName) throws PublishLockException {
 		if (ruleType != null && StringUtils.isNotBlank(username) && StringUtils.isNotBlank(storeName)) {
 			String lock = storeName + "^" + username;
 			lock = lock.intern();
+			String lockOwnerName = null;
+			String lockOwnerStore = null;
 			if (!lockService.get(ruleType).compareAndSet(null, lock)) {
 				String info = getPublishLockInfo(ruleType);
-				username = null;
-				String storeLabel = null;
 				if (StringUtils.isNotBlank(info)) {
 					String[] infoArray = info.split("\\^", 2);
 					if (infoArray.length > 0) {
 						// TODO: get store label
-						storeLabel = infoArray[0];
+						lockOwnerStore = infoArray[0];
 						if (infoArray.length > 1) {
-							username = infoArray[1];
+							lockOwnerName = infoArray[1];
 						}
 					}
 				}
+				logger.info(String.format("Unable to obtain %s publish lock for %s of %s. $s of %s is still holding the lock.", 
+						String.valueOf(ruleType), username, storeName, lockOwnerName, lockOwnerStore));
 				throw new PublishLockException(String.format("%s is currently publishing %s rules for %s, please try again in a while.", 
-						username, ruleType.toString(), storeLabel), username, storeLabel);
+						lockOwnerName, String.valueOf(ruleType), lockOwnerStore), lockOwnerName, lockOwnerStore);
 			}
+			logger.info(String.format("Obtained %s publish lock for %s of %s", String.valueOf(ruleType), username, storeName));
 			return true;
 		}
-		throw new PublishLockException(String.format("Another user is currently publishing %s rules. Please try again in a while.", 
-				ruleType != null ? ruleType.toString(): ""), null, null);
+		logger.error(String.format("Missing Info for lock. RuleType = %s, User = %s, Store = %s", String.valueOf(ruleType), username, storeName));
+		throw new PublishLockException("Please re-login and try to publish again.", null, null);
 	}
 
 	public static String getPublishLockInfo(RuleEntity ruleType) {
@@ -94,15 +95,16 @@ public class UtilityService {
 		return "";
 	}
 
-	public static boolean releasePublishLock(RuleEntity ruleType) {
-		String username = getUsername();
-		String storeName = getStoreName();
+	public static boolean releasePublishLock(RuleEntity ruleType, String username, String storeName) {
 		if (ruleType != null && StringUtils.isNotBlank(username) && StringUtils.isNotBlank(storeName)) {
 			String lock = storeName + "^" + username;
 			lock = lock.intern();
 			boolean released = lockService.get(ruleType).compareAndSet(lock, null);
+			logger.info(String.format("%s %s lock held by %s of %s.", released ? "Released " : "Failed to release ", 
+					String.valueOf(ruleType), username, storeName));
 			return released;
 		}
+		logger.error(String.format("Missing Info for lock. RuleType = %s, User = %s, Store = %s", String.valueOf(ruleType), username, storeName));
 		return false;
 	}
 
