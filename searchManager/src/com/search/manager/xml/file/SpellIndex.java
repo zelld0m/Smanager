b2c00b4aaa4b56de953defa9b5be5f9d70801ffa
jjamolod <jesrome.jamolod@pcmall.com>
2013-05-09 10:15:54 +0800

    - Formatted.

--- searchManager/src/com/search/manager/xml/file/SpellIndex.java
+++ searchManager/src/com/search/manager/xml/file/SpellIndex.java
@@ -23,160 +23,164 @@ import com.search.manager.utility.PropsUtils;
 
 public class SpellIndex {
 
-    private static final Logger logger = LoggerFactory.getLogger(SpellIndex.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(SpellIndex.class);
 
-    private static final String BASE_RULE_DIR = PropsUtils.getValue("rulepath");
-    private static final String TEMP_RULE_DIR = PropsUtils.getValue("temprulepath");
-    private static final String SPELL_FILE = PropsUtils.getValue("spellfile");
-    private static final RuleEntity ENTITY = RuleEntity.SPELL;
+	private static final String BASE_RULE_DIR = PropsUtils.getValue("rulepath");
+	private static final String TEMP_RULE_DIR = PropsUtils
+			.getValue("temprulepath");
+	private static final String SPELL_FILE = PropsUtils.getValue("spellfile");
+	private static final RuleEntity ENTITY = RuleEntity.SPELL;
 
-    private Map<String, SpellRules> rules = new HashMap<String, SpellRules>();
-    private Map<String, String> xmlPath = new HashMap<String, String>();
-    private String dirPath;
-    private String tempDirPath;
+	private Map<String, SpellRules> rules = new HashMap<String, SpellRules>();
+	private Map<String, String> xmlPath = new HashMap<String, String>();
+	private String dirPath;
+	private String tempDirPath;
 
-    public void init() throws Exception {
-        dirPath = new StringBuilder()
-                .append(BASE_RULE_DIR)
-                .append(File.separator)
-                .append(RuleEntity.getValue(ENTITY.getCode()) != null ? RuleEntity.getValue(ENTITY.getCode()) : ENTITY
-                        .name()).toString();
-        tempDirPath = new StringBuilder()
-                .append(TEMP_RULE_DIR)
-                .append(File.separator)
-                .append(RuleEntity.getValue(ENTITY.getCode()) != null ? RuleEntity.getValue(ENTITY.getCode()) : ENTITY
-                        .name()).toString();
+	public void init() throws Exception {
+		dirPath = new StringBuilder()
+				.append(BASE_RULE_DIR)
+				.append(File.separator)
+				.append(RuleEntity.getValue(ENTITY.getCode()) != null ? RuleEntity
+						.getValue(ENTITY.getCode()) : ENTITY.name()).toString();
+		tempDirPath = new StringBuilder()
+				.append(TEMP_RULE_DIR)
+				.append(File.separator)
+				.append(RuleEntity.getValue(ENTITY.getCode()) != null ? RuleEntity
+						.getValue(ENTITY.getCode()) : ENTITY.name()).toString();
 
-        if (!FileUtil.isDirectoryExist(tempDirPath)) {
-            FileUtil.createDirectory(tempDirPath);
-        }
-        if (!FileUtil.isDirectoryExist(dirPath)) {
-            FileUtil.createDirectory(dirPath);
-        }
-    }
+		if (!FileUtil.isDirectoryExist(tempDirPath)) {
+			FileUtil.createDirectory(tempDirPath);
+		}
+		if (!FileUtil.isDirectoryExist(dirPath)) {
+			FileUtil.createDirectory(dirPath);
+		}
+	}
 
-    public void load(String store) throws Exception {
-        if (xmlPath.get(store) == null) {
-            String storeDir = new StringBuilder().append(dirPath).append(File.separator).append(store).toString();
-            String spellFile = new StringBuilder().append(storeDir).append(File.separator).append(SPELL_FILE)
-                    .toString();
-            xmlPath.put(store, spellFile);
+	public void load(String store) throws Exception {
+		if (xmlPath.get(store) == null) {
+			String storeDir = new StringBuilder().append(dirPath)
+					.append(File.separator).append(store).toString();
+			String spellFile = new StringBuilder().append(storeDir)
+					.append(File.separator).append(SPELL_FILE).toString();
+			xmlPath.put(store, spellFile);
 
-            if (!FileUtil.isDirectoryExist(storeDir)) {
-                FileUtil.createDirectory(storeDir);
-            }
+			if (!FileUtil.isDirectoryExist(storeDir)) {
+				FileUtil.createDirectory(storeDir);
+			}
 
-            if (!FileUtil.isExist(spellFile)) {
-                SpellRules storeRules = new SpellRules();
+			if (!FileUtil.isExist(spellFile)) {
+				SpellRules storeRules = new SpellRules();
 
-                storeRules.generateSecondaryIndex();
-                rules.put(store, storeRules);
-            } else {
-                rules.put(store, read(xmlPath.get(store)));
-            }
-        }
-    }
+				storeRules.generateSecondaryIndex();
+				rules.put(store, storeRules);
+			} else {
+				rules.put(store, read(xmlPath.get(store)));
+			}
+		}
+	}
 
-    public void unload(String store) throws Exception {
-        xmlPath.remove(store);
-        rules.remove(store);
-    }
+	public void unload(String store) throws Exception {
+		xmlPath.remove(store);
+		rules.remove(store);
+	}
 
-    public void reload(String store) throws Exception {
-        unload(store);
-        load(store);
-    }
+	public void reload(String store) throws Exception {
+		unload(store);
+		load(store);
+	}
 
-    public SpellRules get(String store) {
-        if (rules.get(store) == null) {
-            try {
-                logger.debug("LOAD START TIME: {}", System.currentTimeMillis());
-                load(store);
-                logger.debug("LOAD END TIME: {}", System.currentTimeMillis());
-            } catch (Exception e) {
-                logger.error("Unable to load spell rules for " + store, e);
-            }
-        }
+	public SpellRules get(String store) {
+		if (rules.get(store) == null) {
+			try {
+				logger.debug("LOAD START TIME: {}", System.currentTimeMillis());
+				load(store);
+				logger.debug("LOAD END TIME: {}", System.currentTimeMillis());
+			} catch (Exception e) {
+				logger.error("Unable to load spell rules for " + store, e);
+			}
+		}
 
-        return rules.get(store);
-    }
+		return rules.get(store);
+	}
 
-    public boolean save(String storeId) {
-        return write(rules.get(storeId), storeId);
-    }
+	public boolean save(String storeId) {
+		return write(rules.get(storeId), storeId);
+	}
 
-    public void destroy() throws Exception {
-        logger.info("Destroying did you mean index.");
-    }
+	public void destroy() throws Exception {
+		logger.info("Destroying did you mean index.");
+	}
 
-    private SpellRules read(String filepath) {
-        FileReader reader = null;
-        SpellRules rules = null;
+	private SpellRules read(String filepath) {
+		FileReader reader = null;
+		SpellRules rules = null;
 
-        try {
-            JAXBContext context = JAXBContext.newInstance(SpellRules.class);
-            Unmarshaller m = context.createUnmarshaller();
+		try {
+			JAXBContext context = JAXBContext.newInstance(SpellRules.class);
+			Unmarshaller m = context.createUnmarshaller();
 
-            reader = new FileReader(filepath);
-            rules = (SpellRules) m.unmarshal(reader);
-            rules.generateSecondaryIndex();
-        } catch (JAXBException e) {
-            logger.error("Unable to create marshaller", e);
-        } catch (Exception e) {
-            logger.error("Unknown error", e);
-        } finally {
-            IOUtils.closeQuietly(reader);
-        }
+			reader = new FileReader(filepath);
+			rules = (SpellRules) m.unmarshal(reader);
+			rules.generateSecondaryIndex();
+		} catch (JAXBException e) {
+			logger.error("Unable to create marshaller", e);
+		} catch (Exception e) {
+			logger.error("Unknown error", e);
+		} finally {
+			IOUtils.closeQuietly(reader);
+		}
 
-        return rules;
-    }
+		return rules;
+	}
 
-    private boolean write(SpellRules rules, String storeId) {
-        boolean success = false;
-        FileWriter writer = null;
+	private boolean write(SpellRules rules, String storeId) {
+		boolean success = false;
+		FileWriter writer = null;
 
-        try {
-            String filePath = getSpellFilePath(storeId, false);
+		try {
+			String filePath = getSpellFilePath(storeId, false);
 
-            // Create backup first.
-            if (FileUtil.isExist(filePath)) {
-                FileUtils.copyFile(new File(filePath), new File(getSpellFilePath(storeId, true)));
-            }
+			// Create backup first.
+			if (FileUtil.isExist(filePath)) {
+				FileUtils.copyFile(new File(filePath), new File(
+						getSpellFilePath(storeId, true)));
+			}
 
-            JAXBContext context = JAXBContext.newInstance(SpellRules.class);
-            Marshaller m = context.createMarshaller();
-            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            writer = new FileWriter(getSpellFilePath(storeId, false));
-            m.marshal(rules, writer);
-            success = true;
-        } catch (JAXBException e) {
-            logger.error("Unable to create marshaller", e);
-        } catch (Exception e) {
-            logger.error("Unknown error", e);
-        } finally {
-            IOUtils.closeQuietly(writer);
-        }
+			JAXBContext context = JAXBContext.newInstance(SpellRules.class);
+			Marshaller m = context.createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			writer = new FileWriter(getSpellFilePath(storeId, false));
+			m.marshal(rules, writer);
+			success = true;
+		} catch (JAXBException e) {
+			logger.error("Unable to create marshaller", e);
+		} catch (Exception e) {
+			logger.error("Unknown error", e);
+		} finally {
+			IOUtils.closeQuietly(writer);
+		}
 
-        return success;
-    }
+		return success;
+	}
 
-    private String getSpellFilePath(String store, boolean isTemp) {
-        StringBuilder builder = new StringBuilder();
+	private String getSpellFilePath(String store, boolean isTemp) {
+		StringBuilder builder = new StringBuilder();
 
-        builder.append(getSpellDirPath(store, isTemp));
-        builder.append(File.separator);
-        builder.append(SPELL_FILE);
+		builder.append(getSpellDirPath(store, isTemp));
+		builder.append(File.separator);
+		builder.append(SPELL_FILE);
 
-        return builder.toString();
-    }
+		return builder.toString();
+	}
 
-    private String getSpellDirPath(String store, boolean isTemp) {
-        StringBuilder builder = new StringBuilder();
+	private String getSpellDirPath(String store, boolean isTemp) {
+		StringBuilder builder = new StringBuilder();
 
-        builder.append(isTemp ? tempDirPath : dirPath);
-        builder.append(File.separator);
-        builder.append(store);
+		builder.append(isTemp ? tempDirPath : dirPath);
+		builder.append(File.separator);
+		builder.append(store);
 
-        return builder.toString();
-    }
+		return builder.toString();
+	}
 }
