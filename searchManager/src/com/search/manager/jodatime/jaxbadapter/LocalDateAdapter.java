b3ea524f0196ada2b2103a2da3e7f755e31fc7b4
mpanalig <mpanalig@pcmall.com>
2013-06-07 09:36:54 +0800

    added null-safe logic

--- searchManager/src/com/search/manager/jodatime/jaxbadapter/LocalDateAdapter.java
+++ searchManager/src/com/search/manager/jodatime/jaxbadapter/LocalDateAdapter.java
@@ -1,14 +1,20 @@
 package com.search.manager.jodatime.jaxbadapter;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.StringUtils;
 import org.joda.time.LocalDate;
 
 public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
 	public LocalDate unmarshal(String v) throws Exception {
-		return new LocalDate(v);
+		if(StringUtils.isNotBlank(v)){
+			return new LocalDate(v);
+		}
+		return null;
 	}
 
 	public String marshal(LocalDate v) throws Exception {
-		return v.toString();
+		return ObjectUtils.toString(v);
 	}
 }
--- searchManager/src/com/search/manager/jodatime/jaxbadapter/LocalTimeAdapter.java
+++ searchManager/src/com/search/manager/jodatime/jaxbadapter/LocalTimeAdapter.java
@@ -2,14 +2,19 @@ package com.search.manager.jodatime.jaxbadapter;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.StringUtils;
 import org.joda.time.LocalTime;
 
 public class LocalTimeAdapter extends XmlAdapter<String, LocalTime> {
 	public LocalTime unmarshal(String v) throws Exception {
-		return new LocalTime(v);
+		if(StringUtils.isNotBlank(v)){
+			return new LocalTime(v);
+		}
+		return null;
 	}
 
 	public String marshal(LocalTime v) throws Exception {
-		return v.toString();
+		return ObjectUtils.toString(v);
 	}
 }
