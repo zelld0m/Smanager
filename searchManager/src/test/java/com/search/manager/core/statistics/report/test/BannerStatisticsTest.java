51bae9da8939503b8ce7deeaa5451a91ef28e46c
Philip <PGutierr@pcmall.com>
2013-07-18 16:22:55 +0800

    - Updated BannerStatisticsTest test class

--- searchManager/src/test/java/com/search/manager/core/statistics/report/test/BannerStatisticsTest.java
+++ searchManager/src/test/java/com/search/manager/core/statistics/report/test/BannerStatisticsTest.java
@@ -1,18 +1,16 @@
 package com.search.manager.core.statistics.report.test;
 
-import java.util.Collection;
+import static org.junit.Assert.assertEquals;
+
 import org.joda.time.DateTime;
 
 import com.search.manager.report.statistics.model.BannerStatistics;
 import com.search.manager.report.statistics.util.BannerStatisticsUtil;
 import com.search.manager.report.statistics.util.PropertiesUtils;
 import java.util.List;
-import java.util.logging.Logger;
-import org.easymock.EasyMock;
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.api.easymock.PowerMock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.core.classloader.annotations.SuppressStaticInitializationFor;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -26,41 +24,65 @@ import org.powermock.reflect.Whitebox;
  * @version 0.0.1
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({PropertiesUtils.class, BannerStatisticsUtil.class})
+@PrepareForTest(BannerStatisticsUtil.class)
 @SuppressStaticInitializationFor(
-        {"com.search.manager.report.statistics.util.PropertiesUtils",
-    "com.search.manager.report.statistics.util.BannerStatisticsUtil"})
+    "com.search.manager.report.statistics.util.BannerStatisticsUtil")
 public class BannerStatisticsTest {
-
-    @SuppressWarnings("unused")
-    private static final Logger logger =
-            Logger.getLogger(BannerStatisticsTest.class.getName());
     private static final String STORE_ID = "pcmall";
+
+    @Before
+    public void initialize() {
+        Whitebox.setInternalState(BannerStatisticsUtil.class,
+                "FILE_LOCATION",
+                "src/test/resources/home/solr/utilities/banner-stats/{0}/{1}/{2}.csv");
+        Whitebox.setInternalState(BannerStatisticsUtil.class,
+                "FILE_CANNOT_BE_FOUND_MESSAGE",
+                PropertiesUtils.getString("File {0} cannot be found."));
+    }
 
     @Test
     public void testGetStatsPerBannerByKeyword() throws Exception {
-        // do the mocking
-        bannerStatisticsTestHelper();
-
         // July 16, 2013 00:00:00
         DateTime startDate = new DateTime(2013, 7, 16, 0, 0);
 
         // July 17, 2013 00:00:00
         DateTime endDate = new DateTime(2013, 7, 17, 0, 0);
+        String keyword = "coy2";
 
-        Collection<BannerStatistics> statsPerBannerByKeyword =
-                BannerStatisticsUtil.getStatsPerBannerByKeyword(STORE_ID,
-                "coy2", startDate.toDate(), endDate.toDate());
+        List<BannerStatistics> statsPerBannerByKeyword =
+                BannerStatisticsUtil.getStatsPerBannerByKeyword(STORE_ID, 
+                keyword, startDate.toDate(), endDate.toDate());
+
+        // expected size=4
+        assertEquals(statsPerBannerByKeyword.size(), 4);
         
-        Assert.assertEquals(statsPerBannerByKeyword.size(), 4);
-        PowerMock.verify(PropertiesUtils.class, BannerStatisticsUtil.class);
+        BannerStatistics bannerStats1 = statsPerBannerByKeyword.get(0);
+        // expected keyword="coy2", clicks=1, impressions=1
+        assertEquals(bannerStats1.getKeyword(), keyword);
+        assertEquals(bannerStats1.getClicks(), 1);
+        assertEquals(bannerStats1.getImpressions(), 1);
+        
+        BannerStatistics bannerStats2 = statsPerBannerByKeyword.get(1);
+        // expected keyword="coy2", clicks=1, impressions=1
+        assertEquals(bannerStats2.getKeyword(), keyword);
+        assertEquals(bannerStats2.getClicks(), 1);
+        assertEquals(bannerStats2.getImpressions(), 1);
+        
+        BannerStatistics bannerStats3 = statsPerBannerByKeyword.get(2);
+        // expected keyword="coy2", clicks=0, impressions=1
+        assertEquals(bannerStats3.getKeyword(), keyword);
+        assertEquals(bannerStats3.getClicks(), 0);
+        assertEquals(bannerStats3.getImpressions(), 1);
+        
+        BannerStatistics bannerStats4 = statsPerBannerByKeyword.get(3);
+        // expected keyword="coy2", clicks=0, impressions=1
+        assertEquals(bannerStats4.getKeyword(), keyword);
+        assertEquals(bannerStats4.getClicks(), 1);
+        assertEquals(bannerStats4.getImpressions(), 1);
     }
 
     @Test
     public void testStatsPerKeywordByMemberId() throws Exception {
-        // do the mocking
-        bannerStatisticsTestHelper();
-
         // July 16, 2013 00:00:00
         DateTime startDate = new DateTime(2013, 7, 16, 0, 0);
 
@@ -74,49 +96,17 @@ public class BannerStatisticsTest {
                 BannerStatisticsUtil.getStatsPerKeywordByMemberId(STORE_ID,
                 memberId, startDate.toDate(), endDate.toDate());
         
-        Assert.assertEquals(statsPerBannerByMemberId.get(0).getMemberId(), 
+        // expected size=1
+        assertEquals(statsPerBannerByMemberId.size(), 1);
+        
+        BannerStatistics bannerStats = statsPerBannerByMemberId.get(0);
+
+        // expected keyword="cable", memberId = "0064dYW1P0OBpaRiyRLD", 
+        // clicks=1, impressions=1
+        assertEquals(bannerStats.getKeyword(), "cable");
+        assertEquals(bannerStats.getMemberId(),
                 memberId);
-        PowerMock.verify(BannerStatisticsUtil.class);
-    }
-
-    /**
-     * Helper method for mocking PropertiesUtils and BannerStatisticsUtil
-     */
-    private void bannerStatisticsTestHelper() {
-        // mock the properties utils
-        mockPropertiesUtilsHelper();
-
-        // replay
-        PowerMock.replay(PropertiesUtils.class, BannerStatisticsUtil.class);
-
-        // helper method for mocking banner statisticsUtil
-        mockBannerStatisticsUtilHelper();
-    }
-
-    /**
-     * Helper method for mocking PropertiesUtils
-     */
-    private void mockPropertiesUtilsHelper() {
-        PowerMock.mockStaticPartial(PropertiesUtils.class,
-                "getString");
-
-        EasyMock.expect(PropertiesUtils.getString("fileLocation")).
-                andReturn(
-                "src/test/resources/home/solr/utilities/banner-stats/{0}/{1}/{2}.csv").
-                anyTimes();
-
-        EasyMock.expect(PropertiesUtils.getString("fileNotFoundMessage")).
-                andReturn("File {0} cannot be found.").anyTimes();
-    }
-
-    /**
-     * Helper method for mocking BannerStatisticsUtil
-     */
-    private void mockBannerStatisticsUtilHelper() {
-        Whitebox.setInternalState(BannerStatisticsUtil.class,
-                "FILE_LOCATION", PropertiesUtils.getString("fileLocation"));
-        Whitebox.setInternalState(BannerStatisticsUtil.class,
-                "FILE_CANNOT_BE_FOUND_MESSAGE",
-                PropertiesUtils.getString("fileNotFoundMessage"));
+        assertEquals(bannerStats.getClicks(), 1);
+        assertEquals(bannerStats.getImpressions(), 1);
     }
 }
