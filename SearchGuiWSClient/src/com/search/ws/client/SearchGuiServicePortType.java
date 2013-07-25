bded6dd4fa12116ac8633ff5405a30fe878fc2a1
jjamolod <jesrome.jamolod@pcmall.com>
2013-07-25 11:19:14 +0800

    - Changed PropsUtil to PropertiesUtils.

--- SearchGuiWSClient/src/com/search/ws/client/SearchGuiServicePortType.java
+++ SearchGuiWSClient/src/com/search/ws/client/SearchGuiServicePortType.java
@@ -1,6 +1,11 @@
 package com.search.ws.client;
 
 public interface SearchGuiServicePortType extends java.rmi.Remote {
-    public com.search.ws.client.AnyType2AnyTypeMapEntry[] deployRulesMap(com.search.webservice.model.TransportList in0) throws java.rmi.RemoteException;
-    public com.search.ws.client.AnyType2AnyTypeMapEntry[] unDeployRulesMap(com.search.webservice.model.TransportList in0) throws java.rmi.RemoteException;
+	public com.search.ws.client.AnyType2AnyTypeMapEntry[] deployRulesMap(
+			com.search.webservice.model.TransportList in0)
+			throws java.rmi.RemoteException;
+
+	public com.search.ws.client.AnyType2AnyTypeMapEntry[] unDeployRulesMap(
+			com.search.webservice.model.TransportList in0)
+			throws java.rmi.RemoteException;
 }
