5573637ab0b249cd87d4078542f1de257ae83567
Philip <PGutierr@pcmall.com>
2013-09-02 14:57:09 +0800

    Added a test class named PropertiesManagerServiceSaveStorePropertiesTest

---
+++ propertiesManager/src/test/java/com/search/properties/manager/service/PropertiesManagerServiceSaveStorePropertiesTest.java
@@ -0,0 +1,24 @@
+package com.search.properties.manager.service;
+
+import com.search.properties.manager.PropertiesManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.annotation.Mock;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ *
+ * @author Philip Mark Gutierrez
+ * @since August 30, 2013
+ * @version 1.0
+ */
+@RunWith(PowerMockRunner.class)
+public class PropertiesManagerServiceSaveStorePropertiesTest {
+    @Mock
+    private PropertiesManager propertiesManager;
+    
+    @Test
+    public void testSaveStoreProperties() {
+        propertiesManager.saveStoreProperties();
+    }
+}
