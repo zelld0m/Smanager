75458d271030b57332b9b44edb164c902e66a71e
Philip <PGutierr@pcmall.com>
2013-09-06 10:54:28 +0800

    Updated the test classes for the additional utility methods

--- propertiesManager/src/test/java/com/search/properties/manager/util/PropertiesManagerUtilTest.java
+++ propertiesManager/src/test/java/com/search/properties/manager/util/PropertiesManagerUtilTest.java
@@ -4,10 +4,14 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import com.search.properties.manager.PropertiesManager;
+import com.search.properties.manager.exception.GroupNotFoundException;
 import com.search.properties.manager.exception.ModuleNotFoundException;
+import com.search.properties.manager.exception.PropertyNotFoundException;
 import com.search.properties.manager.exception.StoreNotFoundException;
 import com.search.properties.manager.exception.StorePropertyNotFoundException;
+import com.search.properties.manager.model.Group;
 import com.search.properties.manager.model.Module;
+import com.search.properties.manager.model.Property;
 import com.search.properties.manager.model.Store;
 import com.search.properties.manager.model.StoreProperties;
 import com.search.properties.manager.model.StorePropertiesFile;
@@ -42,24 +46,28 @@ public class PropertiesManagerUtilTest {
     private StoreProperties storeProperties;
     private Store pcmallCapStore;
     private Store pcmallStore;
+    private Module settingsModuleOnPCMallStore;
     private List<StorePropertiesFile> storePropertiesFiles;
 
     @Before
     public void setup() {
         assertNotNull(propertiesManagerService);
         assertNotNull(propertiesManager);
-        
+
         propertiesManager.setStorePropertiesLocation(
                 "src/test/resources/home/solr/conf/store-properties.xml");
         propertiesManager.setStorePropertiesSaveLocation(
                 "src/test/resources/home/solr/conf");
-        
+
         storeProperties = propertiesManagerService.getStoreProperties();
-        
+
         pcmallCapStore = PropertiesManagerUtil.getStoreById("pcmallcap",
                 storeProperties);
         pcmallStore = PropertiesManagerUtil.getStoreById("pcmall",
                 storeProperties);
+
+        settingsModuleOnPCMallStore = PropertiesManagerUtil.getModuleByName("settings",
+                pcmallStore);
 
         assertNotNull(propertiesReaderService);
         storePropertiesFiles = propertiesReaderService.readAllStorePropertiesFiles(
@@ -78,7 +86,7 @@ public class PropertiesManagerUtilTest {
 
     @Test
     public void testGetParent_Store_Parent_Exists() {
-        assertEquals(pcmallStore, PropertiesManagerUtil.getParent(pcmallCapStore, 
+        assertEquals(pcmallStore, PropertiesManagerUtil.getParent(pcmallCapStore,
                 storeProperties));
     }
 
@@ -111,20 +119,104 @@ public class PropertiesManagerUtilTest {
     public void testGetStorePropertyByName_StoreName_Does_Not_Exists_Throw_StorePropertyNotFoundException() {
         StorePropertiesFile pcmallSettingsStorePropertiesFile = storePropertiesFiles.get(
                 1);
-        PropertiesManagerUtil.getStorePropertyByName("hoopla", 
+        PropertiesManagerUtil.getStorePropertyByName("hoopla",
                 pcmallSettingsStorePropertiesFile);
     }
 
     @Test
     public void testGetModuleByName() {
-        Module settingsModule = PropertiesManagerUtil.getModuleByName("settings", 
-                pcmallStore);
-        assertEquals("settings", settingsModule.getName());
-        assertEquals("Settings", settingsModule.getTitle());
+        assertEquals("settings", settingsModuleOnPCMallStore.getName());
+        assertEquals("Settings", settingsModuleOnPCMallStore.getTitle());
     }
-    
+
     @Test(expected = ModuleNotFoundException.class)
     public void testGetModuleByName_Module_Name_Does_Not_Exists_Throw_ModuleNotFoundException() {
         PropertiesManagerUtil.getModuleByName("potato", pcmallStore);
+    }
+
+    @Test
+    public void testGetGroupByName() {
+        Group bannerAdsFeatureGroup = PropertiesManagerUtil.getGroupByName(
+                "Banner Ads Feature", settingsModuleOnPCMallStore);
+        assertEquals("Banner Ads Feature", bannerAdsFeatureGroup.getName());
+    }
+
+    @Test(expected = GroupNotFoundException.class)
+    public void testGroupGroupByName_Group_Name_Does_Not_Exists_Throw_GroupNotFoundException() {
+        PropertiesManagerUtil.getGroupByName("Avalanche", settingsModuleOnPCMallStore);
+    }
+
+    @Test
+    public void testGetAllGroupsWithoutAName() {
+        List<Group> allGroupsWithoutAName = PropertiesManagerUtil.
+                getAllGroupsWithoutAName(settingsModuleOnPCMallStore);
+        assertEquals("auto_export", allGroupsWithoutAName.get(0).getMembers().
+                get(0).getPropertyId());
+    }
+
+    @Test
+    public void testGroupAllGroupsWithoutAName_No_Empty_Group_Return_Empty_ArrayList() {
+        Module mailModule = PropertiesManagerUtil.getModuleByName("mail", pcmallStore);
+        List<Group> allGroupsWithoutAName = PropertiesManagerUtil.
+                getAllGroupsWithoutAName(mailModule);
+        assertEquals(0, allGroupsWithoutAName.size());
+        assertEquals(java.util.ArrayList.class, allGroupsWithoutAName.getClass());
+    }
+
+    @Test
+    public void testGetPropertyById() {
+        Property siteDomainProperty =
+                PropertiesManagerUtil.getPropertyById("site_domain", settingsModuleOnPCMallStore);
+
+        assertEquals("site_domain", siteDomainProperty.getId());
+        assertEquals("pcm.com", siteDomainProperty.getDefaultValue());
+    }
+
+    @Test(expected = PropertyNotFoundException.class)
+    public void testGetPropertyById_Property_Id_Does_Not_Exists_Throw_PropertyNotFoundException() {
+        PropertiesManagerUtil.getPropertyById("witch_please", settingsModuleOnPCMallStore);
+    }
+
+    @Test
+    public void testContainsModule() {
+        assertEquals(true, PropertiesManagerUtil.containsModule(settingsModuleOnPCMallStore,
+                pcmallCapStore));
+    }
+
+    @Test
+    public void testContainsGroup() {
+        assertEquals(true, PropertiesManagerUtil.containsGroup("Export Feature",
+                settingsModuleOnPCMallStore));
+    }
+
+    @Test
+    public void testContainsGroup_Group_Name_Does_Not_Exists_Return_False() {
+        assertEquals(false, PropertiesManagerUtil.containsGroup("Avalanche",
+                settingsModuleOnPCMallStore));
+    }
+
+    @Test
+    public void testContainsAtLeastAGroupWithoutAName() {
+        assertEquals(true, PropertiesManagerUtil.containsAtLeastAGroupWithoutAName(
+                settingsModuleOnPCMallStore));
+    }
+
+    @Test
+    public void testContainsAtLeastAGroupWithoutAName_Return_False() {
+        Module mailModule = PropertiesManagerUtil.getModuleByName("mail", pcmallStore);
+        assertEquals(false, PropertiesManagerUtil.containsAtLeastAGroupWithoutAName(
+                mailModule));
+    }
+
+    @Test
+    public void testContainsProperty() {
+        assertEquals(true, PropertiesManagerUtil.containsProperty("site_domain",
+                settingsModuleOnPCMallStore));
+    }
+
+    @Test
+    public void testContainsProperty_Return_False() {
+        assertEquals(false, PropertiesManagerUtil.containsProperty("witch_please",
+                settingsModuleOnPCMallStore));
     }
 }
