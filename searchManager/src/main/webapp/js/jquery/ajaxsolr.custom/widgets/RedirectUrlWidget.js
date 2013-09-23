967de9e7cf6c39d90f379992cd15bc603595bf6d
Philip <PGutierr@pcmall.com>
2013-09-23 11:57:47 +0800

    Added support for relative redirect url

--- searchManager/src/main/webapp/js/jquery/ajaxsolr.custom/widgets/RedirectUrlWidget.js
+++ searchManager/src/main/webapp/js/jquery/ajaxsolr.custom/widgets/RedirectUrlWidget.js
@@ -1,17 +1,38 @@
 (function($) {
     AjaxSolr.RedirectUrlWidget = AjaxSolr.AbstractWidget.extend({
+        checkRelativeRedirectUrl: function(urlVal) {
+            for (var i = 0; i < GLOBAL_storeRedirectRelativePath.length; i++) {
+                var prefix = GLOBAL_storeRedirectRelativePath[i];
+                if ($.startsWith(urlVal, prefix)) {
+                    return true;
+                }
+            }
+
+            return false;
+        },
         afterRequest: function() {
             var self = this;
             var direct_hit = self.manager.response.responseHeader.direct_hit;
-            
+
             if (direct_hit !== undefined) {
                 var redirectUrlDivIFrame = $("#redirectUrlDivIFrame");
                 var redirectUrlParam = direct_hit["redirect_url"];
-                
-                redirectUrlDivIFrame.attr("src", redirectUrlParam);
-                
-                $("#redirectUrlTextDiv").html(redirectUrlParam);
-                
+                var urlVal = redirectUrlParam;
+
+                if ($.isBlank(urlVal)) {
+                    jAlert("No redirect URL was specified", "Redirect URL");
+                    return;
+                }
+
+                if ($.isNotBlank(urlVal) && self.checkRelativeRedirectUrl(urlVal)) {
+                    urlVal = GLOBAL_storeDefaultBannerLinkPathProtocol + "://"
+                            + GLOBAL_storeParameters.storeDomains[0] + urlVal;
+                }
+
+                redirectUrlDivIFrame.attr("src", urlVal);
+
+                $("#redirectUrlTextDiv").html(urlVal);
+
                 $("#redirectUrlDiv").dialog({
                     title: "Redirected to Page",
                     width: 800,
