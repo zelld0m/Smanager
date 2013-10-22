399210fd2f61774d229fa47892de70fb164b7660
Philip <PGutierr@pcmall.com>
2013-10-22 14:30:03 +0800

    Added support for the argument constraint "Field"

--- searchManager/src/main/java/com/search/manager/schema/model/bf/FunctionQuery.java
+++ searchManager/src/main/java/com/search/manager/schema/model/bf/FunctionQuery.java
@@ -16,33 +16,33 @@ import com.search.manager.schema.model.bf.Function.ArgumentConstraint;
 
 @DataTransferObject(converter = BeanConverter.class)
 public class FunctionQuery implements FunctionModelComponent {
-	
-	private final static long serialVersionUID = 1L;
-	
-	private final static Logger logger = Logger.getLogger(FunctionQuery.class);
-	
-	private Function function;
-	private List<FunctionModelComponent> arguments = new ArrayList<FunctionModelComponent>();
 
-	public FunctionQuery(Function function) {
-		this.function = function;
-	}
-	
-	public FunctionQuery(Function function, FunctionModelComponent ... args) {
-		this.function = function;
-		for (FunctionModelComponent arg: args) {
-			addArgument(arg);
-		}
-	}
-	
-	public FunctionQuery(String functionName) throws SchemaException {
-		Function function = RelevancyConfig.getInstance().getFunction(functionName);
-		if (function == null) {
-			throw new SchemaException("Unknown function: " + functionName);
-		}
-		this.function = function;
-	}
-	
+    private final static long serialVersionUID = 1L;
+
+    private final static Logger logger = Logger.getLogger(FunctionQuery.class);
+
+    private Function function;
+    private List<FunctionModelComponent> arguments = new ArrayList<FunctionModelComponent>();
+
+    public FunctionQuery(Function function) {
+        this.function = function;
+    }
+
+    public FunctionQuery(Function function, FunctionModelComponent... args) {
+        this.function = function;
+        for (FunctionModelComponent arg : args) {
+            addArgument(arg);
+        }
+    }
+
+    public FunctionQuery(String functionName) throws SchemaException {
+        Function function = RelevancyConfig.getInstance().getFunction(functionName);
+        if (function == null) {
+            throw new SchemaException("Unknown function: " + functionName);
+        }
+        this.function = function;
+    }
+
 //	public FunctionQuery(Schema schema, String functionName, String ... args) throws SchemaException {
 //		this(functionName);
 //
@@ -66,133 +66,135 @@ public class FunctionQuery implements FunctionModelComponent {
 //			addArgument(model);
 //		}
 //	}
-	
-	public Function getFunction() {
-		return function;
-	}
-	
-	public void setFunction(Function function) {
-		this.function = function;
-	}
-	
-	public List<FunctionModelComponent> getArguments() {
-		return arguments;
-	}
+    public Function getFunction() {
+        return function;
+    }
 
-	public void addArguments(FunctionModelComponent ... models) {
-		for (FunctionModelComponent model: models) {
-			arguments.add(model);
-		}
-	}
-	
-	public void addArgument(FunctionModelComponent model) {
-		arguments.add(model);
-	}
+    public void setFunction(Function function) {
+        this.function = function;
+    }
 
-	public void insertArgument(int index, FunctionModelComponent model) {
-		if (index >= 0 && index < arguments.size()) {
-			arguments.add(index, model);
-		}
-		else {
-			arguments.add(model);
-		}
-	}
-	
-	/**
-	 * Remove the argument specified at i
-	 * @param index zero-based index
-	 */
-	public FunctionModelComponent removeArgument(int index) {
-		return (index >= 0 && index < arguments.size()) ? arguments.remove(index) : null;
-	}
+    public List<FunctionModelComponent> getArguments() {
+        return arguments;
+    }
 
-	public FunctionModelComponent getArgument(int index) {
-		return (index >= 0 && index < arguments.size()) ? arguments.get(index) : null;
-	}
-	
-	public List<FunctionModelComponent> getArguments(int index) {
-		return new ArrayList<FunctionModelComponent>(arguments);
-	}
-	
-	private StringBuilder getErrorStringBuilder() {
-		StringBuilder error = new StringBuilder();
-		error.append("Error in function[").append(function.getName()).append("]: ");
-		return error;
-	}
-	
-	@Override
-	public boolean validate() throws SchemaException {
-		StringBuilder error = getErrorStringBuilder();
-		if (arguments.size() < function.getMinArgs() || function.getMaxArgs() != null && arguments.size() > function.getMaxArgs()) {
-			error.append("Incorrect number of arguments. Expected ");
-			if (function.getMinArgs() != null) {
-				error.append(function.getMinArgs());
-				if (function.getMaxArgs() != null) {
-					error.append(" to ");
-				}
-			}
-			if (function.getMaxArgs() != null) {
-				error.append("function.getMaxArgs()");
-			}
-			error.append(".");
-			throw new SchemaException(error.toString());
-		}
-		
-		boolean valid = true;
-		for (int i = 0; i < arguments.size(); i++) {
-			FunctionModelComponent argument = arguments.get(i);
-			ArgumentConstraint constraint = function.getArgumentConstraint(i+1);
-			if (constraint != null) {
-				switch(constraint) {
-					case DATE:
-						valid = argument instanceof DateConstant || argument instanceof Field && ((Field)argument).getGenericType() == GenericType.DATE;
-						break;
-					case NUMERIC_CONSTANT:
-						valid = argument instanceof NumericConstant;
-						break;
-					case NORMAL:
-						valid = argument instanceof FunctionQuery || argument instanceof NumericConstant || argument instanceof Field && ((Field)argument).getGenericType() == GenericType.NUMERIC;
-						break;
-					default:
-						logger.warn("Unknown argument constraint: " + constraint);
-						break;
-				}
-			}
-			
-			if (!valid) {
-				error.append(" Expected ").append(constraint).append(" got ").append(argument.toString());
-				throw new SchemaException(error.toString());
-			}
-			
-			try {
-				valid &= argument.validate();
-			} catch (Exception e) {
-				error.append(" Argument[").append(i+1).append("](").append(e.getMessage()).append(")");
-				throw new SchemaException(error.toString());
-			}
-			
-			if (!valid) {
-				break;
-			}
-		}
-		return valid;
-	}
+    public void addArguments(FunctionModelComponent... models) {
+        for (FunctionModelComponent model : models) {
+            arguments.add(model);
+        }
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append(function.getName())
-			   .append("(");
-		int size = arguments.size();
-		if (size > 0) {
-			builder.append(arguments.get(0).toString());
-			for (int i = 1; i < size; i++) {
-				builder.append(",")
-					   .append(arguments.get(i).toString());
-			}
-		}
-		builder.append(")");
-		return builder.toString();
-	}
-	
+    public void addArgument(FunctionModelComponent model) {
+        arguments.add(model);
+    }
+
+    public void insertArgument(int index, FunctionModelComponent model) {
+        if (index >= 0 && index < arguments.size()) {
+            arguments.add(index, model);
+        } else {
+            arguments.add(model);
+        }
+    }
+
+    /**
+     * Remove the argument specified at i
+     *
+     * @param index zero-based index
+     */
+    public FunctionModelComponent removeArgument(int index) {
+        return (index >= 0 && index < arguments.size()) ? arguments.remove(index) : null;
+    }
+
+    public FunctionModelComponent getArgument(int index) {
+        return (index >= 0 && index < arguments.size()) ? arguments.get(index) : null;
+    }
+
+    public List<FunctionModelComponent> getArguments(int index) {
+        return new ArrayList<FunctionModelComponent>(arguments);
+    }
+
+    private StringBuilder getErrorStringBuilder() {
+        StringBuilder error = new StringBuilder();
+        error.append("Error in function[").append(function.getName()).append("]: ");
+        return error;
+    }
+
+    @Override
+    public boolean validate() throws SchemaException {
+        StringBuilder error = getErrorStringBuilder();
+        if (arguments.size() < function.getMinArgs() || function.getMaxArgs() != null && arguments.size() > function.getMaxArgs()) {
+            error.append("Incorrect number of arguments. Expected ");
+            if (function.getMinArgs() != null) {
+                error.append(function.getMinArgs());
+                if (function.getMaxArgs() != null) {
+                    error.append(" to ");
+                }
+            }
+            if (function.getMaxArgs() != null) {
+                error.append("function.getMaxArgs()");
+            }
+            error.append(".");
+            throw new SchemaException(error.toString());
+        }
+
+        boolean valid = true;
+        for (int i = 0; i < arguments.size(); i++) {
+            FunctionModelComponent argument = arguments.get(i);
+            ArgumentConstraint constraint = function.getArgumentConstraint(i + 1);
+            if (constraint != null) {
+                switch (constraint) {
+                    case DATE:
+                        valid = argument instanceof DateConstant || argument instanceof Field && ((Field) argument).getGenericType() == GenericType.DATE;
+                        break;
+                    case NUMERIC_CONSTANT:
+                        valid = argument instanceof NumericConstant;
+                        break;
+                    case FIELD:
+                        valid = argument instanceof Field;
+                        break;
+                    case NORMAL:
+                        valid = argument instanceof FunctionQuery || argument instanceof NumericConstant || argument instanceof Field && ((Field) argument).getGenericType() == GenericType.NUMERIC;
+                        break;
+                    default:
+                        logger.warn("Unknown argument constraint: " + constraint);
+                        break;
+                }
+            }
+
+            if (!valid) {
+                error.append(" Expected ").append(constraint).append(" got ").append(argument.toString());
+                throw new SchemaException(error.toString());
+            }
+
+            try {
+                valid &= argument.validate();
+            } catch (Exception e) {
+                error.append(" Argument[").append(i + 1).append("](").append(e.getMessage()).append(")");
+                throw new SchemaException(error.toString());
+            }
+
+            if (!valid) {
+                break;
+            }
+        }
+        return valid;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(function.getName())
+                .append("(");
+        int size = arguments.size();
+        if (size > 0) {
+            builder.append(arguments.get(0).toString());
+            for (int i = 1; i < size; i++) {
+                builder.append(",")
+                        .append(arguments.get(i).toString());
+            }
+        }
+        builder.append(")");
+        return builder.toString();
+    }
+
 }
