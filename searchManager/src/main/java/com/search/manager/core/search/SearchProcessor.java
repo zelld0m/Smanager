8a68e37229d3156f880b0bfed69dc8d0251f844f
jjamolod <jesrome.jamolod@pcmall.com>
2013-10-23 17:02:11 +0800

    - Initial SearchProcessor.

--- searchManager/src/main/java/com/search/manager/core/search/SearchProcessor.java
+++ searchManager/src/main/java/com/search/manager/core/search/SearchProcessor.java
@@ -1,29 +1,9 @@
 package com.search.manager.core.search;
 
-import java.util.List;
+public interface SearchProcessor {
 
-public abstract class SearchProcessor {
+	SearchResult<?> processSearch(Search search) throws Exception;
 
-	protected abstract String generateStrQuery(Search search) throws Exception;
-
-	protected abstract String generateSelectClause(Search search)
-			throws Exception;
-
-	protected abstract String generateFromClause(Search search)
-			throws Exception;
-
-	protected abstract String generateWhereClause(Search search)
-			throws Exception;
-
-	protected abstract String generateOrderByClause(Search search)
-			throws Exception;
-
-	protected abstract List<Field> checkAndCleanFields(List<Field> fields)
-			throws Exception;
-
-	protected abstract List<Filter> checkAndCleanFilters(List<Filter> filters)
-			throws Exception;
-
-	protected abstract List<Sort> checkAndCleanSorts(List<Sort> sorts)
-			throws Exception;
+	String generateStrQuery(Search search) throws Exception;
+	
 }
