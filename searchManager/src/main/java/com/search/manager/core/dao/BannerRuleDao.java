0eb1c83f9a8ab654292fb19b482de6a505153110
jjamolod <jesrome.jamolod@pcmall.com>
2013-10-24 14:40:17 +0800

    - Initial BannerRuleDao.

---
+++ searchManager/src/main/java/com/search/manager/core/dao/BannerRuleDao.java
@@ -0,0 +1,9 @@
+package com.search.manager.core.dao;
+
+import com.search.manager.model.BannerRule;
+
+public interface BannerRuleDao extends GenericDao<BannerRule> {
+
+	// Add BannerRuleDao specific method here...
+
+}
