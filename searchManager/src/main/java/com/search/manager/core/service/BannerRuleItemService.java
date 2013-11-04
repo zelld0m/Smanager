e47c2245b8d672ea70fe3fdd403095eb4e091c65
jjamolod <jesrome.jamolod@pcmall.com>
2013-11-04 12:06:13 +0800

    - Initial BannerRuleItemService and GenericService.

---
+++ searchManager/src/main/java/com/search/manager/core/service/BannerRuleItemService.java
@@ -0,0 +1,9 @@
+package com.search.manager.core.service;
+
+import com.search.manager.core.model.BannerRuleItem;
+
+public interface BannerRuleItemService extends GenericService<BannerRuleItem> {
+
+	// Add BannerRuleItemService specific method here...
+	
+}
---
+++ searchManager/src/main/java/com/search/manager/core/service/GenericService.java
@@ -0,0 +1,17 @@
+package com.search.manager.core.service;
+
+import com.search.manager.core.exception.CoreServiceException;
+import com.search.manager.core.search.Search;
+import com.search.manager.core.search.SearchResult;
+
+public interface GenericService<T> {
+	
+	T add(T model) throws CoreServiceException;
+
+	T update(T model) throws CoreServiceException;
+
+	boolean delete(T model) throws CoreServiceException;
+
+	SearchResult<T> search(Search search) throws CoreServiceException;
+	
+}
