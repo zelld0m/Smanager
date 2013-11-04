ea93f8d6e779528355199b4ee5a4fb9d13277a89
jjamolod <jesrome.jamolod@pcmall.com>
2013-11-04 12:11:01 +0800

    - Using CoreDaoException.

--- searchManager/src/main/java/com/search/manager/core/dao/solr/GenericDaoSolrImpl.java
+++ searchManager/src/main/java/com/search/manager/core/dao/solr/GenericDaoSolrImpl.java
@@ -1,15 +1,18 @@
 package com.search.manager.core.dao.solr;
 
+import java.io.IOException;
 import java.lang.reflect.ParameterizedType;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.response.UpdateResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.search.manager.core.SolrCore;
 import com.search.manager.core.dao.GenericDao;
+import com.search.manager.core.exception.CoreDaoException;
 import com.search.manager.core.search.Search;
 import com.search.manager.core.search.SearchProcessor;
 import com.search.manager.core.search.SearchResult;
@@ -33,47 +36,72 @@ public abstract class GenericDaoSolrImpl<T> implements GenericDao<T> {
 	}
 
 	@Override
-	public Class<T> getModelClass() throws Exception {
+	public Class<T> getModelClass() throws CoreDaoException {
 		return this.modelClass;
 	}
 
-	private SolrServer getSolrServer() throws Exception {
+	private SolrServer getSolrServer() throws CoreDaoException {
 		String core;
 		SolrCore solrCore = (SolrCore) modelClass.getAnnotation(SolrCore.class);
 
 		if (solrCore != null) {
 			core = solrCore.name();
-			return solrServerFactory.getCoreInstance(core).getHttpSolrServer();
+			try {
+				return solrServerFactory.getCoreInstance(core).getHttpSolrServer();
+			} catch (SolrServerException e) {
+				throw new CoreDaoException(e);
+			}
 		}
 
 		return null;
 	}
 
 	@Override
-	public T add(T model) throws Exception {
-		getSolrServer().addBean(model, 10000);
+	public T add(T model) throws CoreDaoException {
+		try {
+			getSolrServer().addBean(model, 10000);
+		} catch (IOException e) {
+			throw new CoreDaoException(e);
+		} catch (SolrServerException e) {
+			throw new CoreDaoException(e);
+		} catch (Exception e) {
+			throw new CoreDaoException(e);
+		}
 		return model;
 	}
 
 	@Override
-	public T update(T model) throws Exception {
+	public T update(T model) throws CoreDaoException {
 		return add(model);
 	}
 
 	@Override
-	public boolean delete(T model) throws Exception {
+	public boolean delete(T model) throws CoreDaoException {
 		Search search = generateQuery(model);
 		if (search != null) {
-			String query = searchProcessor.generateStrQuery(search);
+			String query;
+			try {
+				query = searchProcessor.generateStrQuery(search);
+			} catch (Exception e) {
+				throw new CoreDaoException(e);
+			}
 			// remove ../select?
 			query = query.substring(query.indexOf('?') + 1, query.length());
 			if (StringUtils.isNotBlank(query)) {
 				// deleteByQuery features of solr don't support filter queries
 				query = query.replace("q=*:*&fq=", "");
-				UpdateResponse updateResponse = getSolrServer().deleteByQuery(
-						query);
-				getSolrServer().commit();
-				return updateResponse.getStatus() == 0 ? true : false;
+				
+				try {
+					UpdateResponse updateResponse = getSolrServer().deleteByQuery(
+							query);
+					getSolrServer().commit();
+					return updateResponse.getStatus() == 0 ? true : false;
+				} catch (SolrServerException e) {
+					throw new CoreDaoException(e);
+				} catch (IOException e) {
+					throw new CoreDaoException(e);
+				}
+				
 			}
 		}
 
@@ -82,8 +110,12 @@ public abstract class GenericDaoSolrImpl<T> implements GenericDao<T> {
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public SearchResult<T> search(Search search) throws Exception {
-		return (SearchResult<T>) searchProcessor.processSearch(search);
+	public SearchResult<T> search(Search search) throws CoreDaoException {
+		try {
+			return (SearchResult<T>) searchProcessor.processSearch(search);
+		} catch (Exception e) {
+			throw new CoreDaoException(e);
+		}
 	}
 
 }
