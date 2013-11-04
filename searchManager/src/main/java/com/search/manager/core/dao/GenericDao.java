688d0e850ddeb8f0a8c3d57c2980feacefc06ddf
jjamolod <jesrome.jamolod@pcmall.com>
2013-11-04 12:09:27 +0800

    - Added CoreDaoException.

--- searchManager/src/main/java/com/search/manager/core/dao/GenericDao.java
+++ searchManager/src/main/java/com/search/manager/core/dao/GenericDao.java
@@ -1,18 +1,19 @@
 package com.search.manager.core.dao;
 
+import com.search.manager.core.exception.CoreDaoException;
 import com.search.manager.core.search.Search;
 import com.search.manager.core.search.SearchResult;
 
 public interface GenericDao<T> {
 
-	Class<T> getModelClass() throws Exception;
+	Class<T> getModelClass() throws CoreDaoException;
 
-	T add(T model) throws Exception;
+	T add(T model) throws CoreDaoException;
 
-	T update(T model) throws Exception;
+	T update(T model) throws CoreDaoException;
 
-	boolean delete(T model) throws Exception;
+	boolean delete(T model) throws CoreDaoException;
 
-	SearchResult<T> search(Search search) throws Exception;
+	SearchResult<T> search(Search search) throws CoreDaoException;
 
 }
