706d5023bf6f0339ffea5a68c634e5b048be2ec6
jjamolod <jesrome.jamolod@pcmall.com>
2013-11-04 12:38:42 +0800

    - Added SearchResult(List<T> result, int totalCount).

--- searchManager/src/main/java/com/search/manager/core/search/SearchResult.java
+++ searchManager/src/main/java/com/search/manager/core/search/SearchResult.java
@@ -1,15 +1,25 @@
 package com.search.manager.core.search;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.poi.ss.formula.functions.T;
-
-public class SearchResult implements Serializable {
+public class SearchResult<T> implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	protected List<T> result;
-	protected int totalCount = -1;
+
+	private List<T> result;
+	private int totalCount;
+
+	public SearchResult() {
+		this.result = new ArrayList<T>();
+		this.totalCount = -1;
+	}
+
+	public SearchResult(List<T> result, int totalCount) {
+		this.result = result;
+		this.totalCount = totalCount;
+	}
 
 	public List<T> getResult() {
 		return result;
