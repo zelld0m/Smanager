07f0a4c05f671250b82e4a86c0d2d8f5e0051ca3
jjamolod <jesrome.jamolod@pcmall.com>
2013-11-04 12:17:34 +0800

    - Changed Exception to CoreSearchException.

--- searchManager/src/main/java/com/search/manager/core/search/BaseSearchProcessor.java
+++ searchManager/src/main/java/com/search/manager/core/search/BaseSearchProcessor.java
@@ -2,30 +2,32 @@ package com.search.manager.core.search;
 
 import java.util.List;
 
+import com.search.manager.core.exception.CoreSearchException;
+
 public abstract class BaseSearchProcessor {
-	
-	protected abstract String generateStrQuery(Search search) throws Exception;
+
+	protected abstract String generateStrQuery(Search search)
+			throws CoreSearchException;
 
 	protected abstract String generateSelectClause(Search search)
-			throws Exception;
+			throws CoreSearchException;
 
 	protected abstract String generateFromClause(Search search)
-			throws Exception;
+			throws CoreSearchException;
 
 	protected abstract String generateWhereClause(Search search)
-			throws Exception;
+			throws CoreSearchException;
 
 	protected abstract String generateOrderByClause(Search search)
-			throws Exception;
-	
-	
+			throws CoreSearchException;
+
 	protected abstract List<Field> checkAndCleanFields(List<Field> fields)
-			throws Exception;
+			throws CoreSearchException;
 
 	protected abstract List<Filter> checkAndCleanFilters(List<Filter> filters)
-			throws Exception;
+			throws CoreSearchException;
 
 	protected abstract List<Sort> checkAndCleanSorts(List<Sort> sorts)
-			throws Exception;
-	
+			throws CoreSearchException;
+
 }
