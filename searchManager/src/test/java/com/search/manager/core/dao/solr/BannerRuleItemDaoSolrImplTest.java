890f61e796e08fdb1f423d2a6715ec108b52d5ea
jjamolod <jesrome.jamolod@pcmall.com>
2013-11-06 14:51:29 +0800

    - Used test data.

--- searchManager/src/test/java/com/search/manager/core/dao/solr/BannerRuleItemDaoSolrImplTest.java
+++ searchManager/src/test/java/com/search/manager/core/dao/solr/BannerRuleItemDaoSolrImplTest.java
@@ -7,11 +7,11 @@ import java.util.List;
 
 import org.joda.time.DateTime;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import com.search.manager.BannerRuleItemTestData;
 import com.search.manager.core.BaseIntegrationTest;
 import com.search.manager.core.dao.BannerRuleItemDao;
 import com.search.manager.core.exception.CoreDaoException;
@@ -30,47 +30,11 @@ public class BannerRuleItemDaoSolrImplTest extends BaseIntegrationTest {
 	@Qualifier("bannerRuleItemDaoSolr")
 	private BannerRuleItemDao bannerRuleItemDao;
 
-	// Test Data
-	private BannerRuleItem bannerRuleItem;
-	private BannerRule bannerRule;
-	private ImagePath imagePath;
-	private DateTime startDate;
-	private DateTime endDate;
-
-	private DateTime startDateUpdated = new DateTime();
-	private DateTime endDateUpdated = new DateTime();
-
-	@Before
-	public void initData() {
-		bannerRuleItem = new BannerRuleItem();
-		bannerRule = new BannerRule();
-		imagePath = new ImagePath();
-
-		startDate = new DateTime();
-		endDate = new DateTime();
-
-		bannerRule.setStoreId("pcmall");
-		bannerRule.setRuleId("ruleId1234");
-		bannerRule.setRuleName("ruleName");
-
-		imagePath.setId("imagePathId1234");
-		imagePath.setPath("path");
-		imagePath.setSize("size");
-		imagePath.setPathType(ImagePathType.IMAGE_LINK);
-		imagePath.setAlias("alias");
-
-		bannerRuleItem.setRule(bannerRule);
-		bannerRuleItem.setMemberId("memberId1234");
-		bannerRuleItem.setPriority(1);
-		bannerRuleItem.setStartDate(startDate);
-		bannerRuleItem.setEndDate(endDate);
-		bannerRuleItem.setImageAlt("imageAlt");
-		bannerRuleItem.setLinkPath("linkPath");
-		bannerRuleItem.setOpenNewWindow(false);
-		bannerRuleItem.setDescription("description");
-		bannerRuleItem.setDisabled(false);
-		bannerRuleItem.setImagePath(imagePath);
-	}
+	// test data
+	private DateTime origStartDate = BannerRuleItemTestData
+			.getExistingBannerRuleItem().getStartDate();
+	private DateTime origEndDate = BannerRuleItemTestData
+			.getExistingBannerRuleItem().getEndDate();
 
 	@Test
 	public void daoWiringTest() {
@@ -80,12 +44,18 @@ public class BannerRuleItemDaoSolrImplTest extends BaseIntegrationTest {
 	@Test
 	public void addTest() throws CoreDaoException {
 		BannerRuleItem addedBannerRuleItem = bannerRuleItemDao
-				.add(bannerRuleItem);
+				.add(BannerRuleItemTestData.getExistingBannerRuleItem());
 		assertNotNull(addedBannerRuleItem);
 	}
 
 	@Test
-	public void updateTest() throws CoreDaoException {
+	public void updateTest() throws CoreDaoException, InterruptedException {
+		BannerRuleItem bannerRuleItem = BannerRuleItemTestData
+				.getExistingBannerRuleItem();
+		BannerRule bannerRule = bannerRuleItem.getRule();
+		ImagePath imagePath = bannerRuleItem.getImagePath();
+
+		// Update fields
 		// bannerRule.setStoreId("pcmall");
 		// bannerRule.setRuleId("ruleId1234");
 		bannerRule.setRuleName("ruleName - updated");
@@ -99,8 +69,8 @@ public class BannerRuleItemDaoSolrImplTest extends BaseIntegrationTest {
 		bannerRuleItem.setRule(bannerRule);
 		// bannerRuleItemUpdated.setMemberId("memberId1234");
 		bannerRuleItem.setPriority(2);
-		bannerRuleItem.setStartDate(startDateUpdated);
-		bannerRuleItem.setEndDate(endDateUpdated);
+		bannerRuleItem.setStartDate(new DateTime().plus(1));
+		bannerRuleItem.setEndDate(new DateTime().plus(15));
 		bannerRuleItem.setImageAlt("imageAlt - updated");
 		bannerRuleItem.setLinkPath("linkPath - updated");
 		bannerRuleItem.setOpenNewWindow(true);
@@ -108,8 +78,28 @@ public class BannerRuleItemDaoSolrImplTest extends BaseIntegrationTest {
 		bannerRuleItem.setDisabled(true);
 		bannerRuleItem.setImagePath(imagePath);
 
-		// update
 		bannerRuleItem = bannerRuleItemDao.update(bannerRuleItem);
+
+		Thread.sleep(12000);
+		
+		// Test successful update
+		assertNotNull(bannerRuleItem);
+		// Assert.assertTrue(2 == bannerRuleItem.getPriority());
+		Assert.assertFalse(origStartDate.isEqual(bannerRuleItem.getStartDate()));
+		Assert.assertFalse(origEndDate.isEqual(bannerRuleItem.getEndDate()
+				.plus(30)));
+		Assert.assertEquals("imageAlt - updated", bannerRuleItem.getImageAlt());
+		Assert.assertEquals("linkPath - updated", bannerRuleItem.getLinkPath());
+		Assert.assertTrue(true == bannerRuleItem.getOpenNewWindow());
+		Assert.assertEquals("description - updated",
+				bannerRuleItem.getDescription());
+		Assert.assertTrue(true == bannerRuleItem.getDisabled());
+
+		// Revert
+		bannerRuleItem = bannerRuleItemDao.update(BannerRuleItemTestData
+				.getExistingBannerRuleItem());
+
+		// Test successful revert
 		assertNotNull(bannerRuleItem);
 	}
 
@@ -189,44 +179,50 @@ public class BannerRuleItemDaoSolrImplTest extends BaseIntegrationTest {
 	public void searchWithFilterTest() throws CoreDaoException {
 		Search search = new Search(BannerRuleItem.class);
 
-		bannerRuleItem.setRule(bannerRule);
-		// bannerRuleItemUpdated.setMemberId("memberId1234");
-		bannerRuleItem.setPriority(2);
-		bannerRuleItem.setStartDate(startDateUpdated);
-		bannerRuleItem.setEndDate(endDateUpdated);
-		bannerRuleItem.setImageAlt("imageAlt - updated");
-		bannerRuleItem.setLinkPath("linkPath - updated");
-		bannerRuleItem.setOpenNewWindow(true);
-		bannerRuleItem.setDescription("description - updated");
-		bannerRuleItem.setDisabled(true);
-		bannerRuleItem.setImagePath(imagePath);
-
 		// Filters
 		List<Filter> filters = new ArrayList<Filter>();
+
 		// banner rule field
-		filters.add(new Filter("store", "pcmall"));
-		filters.add(new Filter("ruleId", "ruleId1234"));
-		filters.add(new Filter("ruleName", "ruleName - updated"));
+		filters.add(new Filter("store", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getRule().getStoreId()));
+		filters.add(new Filter("ruleId", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getRule().getRuleId()));
+		filters.add(new Filter("ruleName", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getRule().getRuleName()));
 
 		// image path field
-		filters.add(new Filter("imagePathId", "imagePathId1234 - updated"));
-		filters.add(new Filter("path", "path - updated"));
-		filters.add(new Filter("size", "size - updated"));
-		filters.add(new Filter("pathType", ImagePathType.UPLOAD_LINK));
-		filters.add(new Filter("alias", "alias - updated"));
+		filters.add(new Filter("imagePathId", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getImagePath().getId()));
+		filters.add(new Filter("path", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getImagePath().getPath()));
+		filters.add(new Filter("size", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getImagePath().getSize()));
+		filters.add(new Filter("pathType", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getImagePath().getPathType()));
+		filters.add(new Filter("alias", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getImagePath().getAlias()));
 
 		// banner rule item field
-		filters.add(new Filter("memberId", "memberId1234"));
-		filters.add(new Filter("priority", 2));
-		filters.add(new Filter("startDate", startDateUpdated,
+		filters.add(new Filter("memberId", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getMemberId()));
+		filters.add(new Filter("priority", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getPriority()));
+		filters.add(new Filter("startDate", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getStartDate(),
 				FilterOperator.LESS_OR_EQUAL));
-		filters.add(new Filter("endDate", endDateUpdated,
+		filters.add(new Filter("endDate", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getEndDate(),
 				FilterOperator.LESS_OR_EQUAL));
-		filters.add(new Filter("imageAlt", "imageAlt - updated"));
-		filters.add(new Filter("linkPath", "linkPath - updated"));
-		filters.add(new Filter("openNewWindow", true));
-		filters.add(new Filter("description", "description - updated"));
-		filters.add(new Filter("disabled", true));
+		filters.add(new Filter("imageAlt", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getImageAlt()));
+		filters.add(new Filter("linkPath", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getLinkPath()));
+		filters.add(new Filter("openNewWindow", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getOpenNewWindow()));
+		filters.add(new Filter("description", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getDescription()));
+		filters.add(new Filter("disabled", BannerRuleItemTestData
+				.getExistingBannerRuleItem().getDisabled()));
 
 		search.addFilters(filters);
 
