20e5e90616802bc71806ac1cbc84f952ee06e07e
jjamolod <jesrome.jamolod@pcmall.com>
2013-11-06 14:50:30 +0800

    - Initialize PropertiesUtils and ConfigManager.

--- searchManager/src/test/java/com/search/manager/core/BaseIntegrationTest.java
+++ searchManager/src/test/java/com/search/manager/core/BaseIntegrationTest.java
@@ -4,8 +4,17 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({ "spring-solr-test-context.xml" })
-public class BaseIntegrationTest {
+import com.search.manager.utility.PropertiesUtils;
+import com.search.ws.ConfigManager;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath:spring/spring-context-test.xml",
+		"classpath:spring/spring-solr-context-test.xml",
+		"classpath:spring/spring-datasource-test.xml" })
+public class BaseIntegrationTest {
+	
+	public BaseIntegrationTest() {
+		PropertiesUtils.initPropertiesConfig("C:\\home\\solr\\conf\\globalvar.properties");
+		ConfigManager.getInstance("C:\\home\\solr\\conf\\solr.xml");
+	}
 }
