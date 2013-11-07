5162d422839f7524e9115d144cdfcbfb06a7acf9
jjamolod <jesrome.jamolod@pcmall.com>
2013-11-07 10:56:17 +0800

    - Formatted.

--- searchManager/src/main/java/com/search/manager/core/service/BannerRuleItemService.java
+++ searchManager/src/main/java/com/search/manager/core/service/BannerRuleItemService.java
@@ -5,5 +5,5 @@ import com.search.manager.core.model.BannerRuleItem;
 public interface BannerRuleItemService extends GenericService<BannerRuleItem> {
 
 	// Add BannerRuleItemService specific method here...
-	
+
 }
