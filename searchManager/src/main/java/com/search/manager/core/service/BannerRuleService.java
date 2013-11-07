c3eb19fc0983e2d07212317fd30395820ae6f68f
jjamolod <jesrome.jamolod@pcmall.com>
2013-11-07 10:55:16 +0800

    - Initial BannerRuleService and ImagePathService.

---
+++ searchManager/src/main/java/com/search/manager/core/service/BannerRuleService.java
@@ -0,0 +1,9 @@
+package com.search.manager.core.service;
+
+import com.search.manager.core.model.BannerRule;
+
+public interface BannerRuleService extends GenericService<BannerRule> {
+
+	// Add BannerRuleService specific method here...
+
+}
---
+++ searchManager/src/main/java/com/search/manager/core/service/ImagePathService.java
@@ -0,0 +1,9 @@
+package com.search.manager.core.service;
+
+import com.search.manager.core.model.ImagePath;
+
+public interface ImagePathService extends GenericService<ImagePath> {
+
+	// Add ImagePathService specific method here...
+
+}
