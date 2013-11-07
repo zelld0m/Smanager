9cdc0c1ecb0165c9680407651407f1c9d08f78dc
jjamolod <jesrome.jamolod@pcmall.com>
2013-11-07 10:51:46 +0800

    - Fixed jode time format.

--- searchManager/src/main/java/com/search/manager/core/util/JsonUtil.java
+++ searchManager/src/main/java/com/search/manager/core/util/JsonUtil.java
@@ -1,6 +1,5 @@
 package com.search.manager.core.util;
 
-import org.codehaus.jackson.map.ext.JodaDeserializers.DateTimeDeserializer;
 import org.joda.time.DateTime;
 
 import com.google.gson.Gson;
@@ -9,9 +8,9 @@ import com.google.gson.GsonBuilder;
 public class JsonUtil {
 
 	public static String toJson(Object object) {
-		// TODO Joda time format
-		// Gson gson = new GsonBuilder().registerTypeAdapter(DateTime.class, new DateTimeDeserializer()).setDateFormat("MM/dd/yyyy hh:mm:ss").create();
-		Gson gson = new Gson();
+		// Gson gson = new Gson();
+		Gson gson = new GsonBuilder().registerTypeAdapter(DateTime.class,
+				new DateTimeTypeConverter()).create();
 
 		return gson.toJson(object);
 	}
