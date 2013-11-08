93f4451aab16ece3e3ad74258d6e2da80405daa0
jjamolod <jesrome.jamolod@pcmall.com>
2013-11-08 09:13:54 +0800

    - Added logger.

--- searchManager/src/test/java/com/search/manager/core/search/processor/SolrSearchProcessorTest.java
+++ searchManager/src/test/java/com/search/manager/core/search/processor/SolrSearchProcessorTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.util.List;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
@@ -15,37 +17,40 @@ import com.search.manager.core.search.SearchProcessor;
 import com.search.manager.core.search.SearchResult;
 
 public class SolrSearchProcessorTest extends BaseIntegrationTest {
-	
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(SolrSearchProcessorTest.class);
+
 	@Autowired
 	@Qualifier("solrSearchProcessor")
-	private SearchProcessor solrSearchProcessor;
-	
+	private SearchProcessor searchProcessor;
+
 	@Test
 	public void SearchProcessorWiringTest() {
-		assertNotNull(solrSearchProcessor);
-	} 
-	
+		assertNotNull(searchProcessor);
+	}
+
 	@Test
 	public void searchTest() {
 		Search search = new Search(BannerRuleItem.class);
-		
+
 		try {
 			@SuppressWarnings("unchecked")
-			SearchResult<BannerRuleItem> searchResult = (SearchResult<BannerRuleItem>) solrSearchProcessor.processSearch(search);
-			
-			if(searchResult != null) {
-				System.out.println("size = " + searchResult.getTotalCount());
-				
+			SearchResult<BannerRuleItem> searchResult = (SearchResult<BannerRuleItem>) searchProcessor
+					.processSearch(search);
+
+			if (searchResult != null) {
+				logger.info("size = " + searchResult.getTotalCount());
+
 				List<BannerRuleItem> bannerRuleItems = searchResult.getResult();
-				
-				for(BannerRuleItem bannerRuleItem : bannerRuleItems) {
-					System.out.println(bannerRuleItem.toJson());
+
+				for (BannerRuleItem bannerRuleItem : bannerRuleItems) {
+					logger.info(bannerRuleItem.toJson());
 				}
 			}
 		} catch (Exception e) {
-			System.out.println(e);
+			logger.error(e.getMessage());
 		}
 	}
-	
-	
+
 }
