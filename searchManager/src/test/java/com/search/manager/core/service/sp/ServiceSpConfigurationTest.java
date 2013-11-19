091e77e98820d4421a8d14e0d8bb9023ff8a6184
jjamolod <jesrome.jamolod@pcmall.com>
2013-11-19 11:36:25 +0800

    - Added ruleStatusServiceSp and auditTrailServiceSp service wiring test.

--- searchManager/src/test/java/com/search/manager/core/service/sp/ServiceSpConfigurationTest.java
+++ searchManager/src/test/java/com/search/manager/core/service/sp/ServiceSpConfigurationTest.java
@@ -7,12 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.search.manager.core.BaseIntegrationTest;
+import com.search.manager.core.service.AuditTrailService;
 import com.search.manager.core.service.BannerRuleItemService;
 import com.search.manager.core.service.BannerRuleService;
 import com.search.manager.core.service.ImagePathService;
+import com.search.manager.core.service.RuleStatusService;
 
 public class ServiceSpConfigurationTest extends BaseIntegrationTest {
 
+	@Autowired
+	@Qualifier("ruleStatusServiceSp")
+	private RuleStatusService ruleStatusService;
+	@Autowired
+	@Qualifier("auditTrailServiceSp")
+	private AuditTrailService auditTrailService;
+	// rule
 	@Autowired
 	@Qualifier("bannerRuleServiceSp")
 	private BannerRuleService bannerRuleService;
@@ -25,6 +34,9 @@ public class ServiceSpConfigurationTest extends BaseIntegrationTest {
 
 	@Test
 	public void testWiring() {
+		assertNotNull(ruleStatusService);
+		assertNotNull(auditTrailService);
+		// rule
 		assertNotNull(bannerRuleService);
 		assertNotNull(bannerRuleItemService);
 		assertNotNull(imagePathService);
