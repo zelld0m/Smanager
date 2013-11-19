4f26880c78c8d4b791519f1768667977480ab79b
jjamolod <jesrome.jamolod@pcmall.com>
2013-11-19 11:34:47 +0800

    - Added ruleStatusDaoSp and auditTrailDaoSp wiring test.

--- searchManager/src/test/java/com/search/manager/core/dao/sp/DaoSpConfigurationTest.java
+++ searchManager/src/test/java/com/search/manager/core/dao/sp/DaoSpConfigurationTest.java
@@ -1,18 +1,27 @@
 package com.search.manager.core.dao.sp;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.search.manager.core.BaseIntegrationTest;
+import com.search.manager.core.dao.AuditTrailDao;
 import com.search.manager.core.dao.BannerRuleDao;
 import com.search.manager.core.dao.BannerRuleItemDao;
 import com.search.manager.core.dao.ImagePathDao;
+import com.search.manager.core.dao.RuleStatusDao;
 
 public class DaoSpConfigurationTest extends BaseIntegrationTest {
 
+	@Autowired
+	@Qualifier("ruleStatusDaoSp")
+	private RuleStatusDao ruleStatusDao;
+	@Autowired
+	@Qualifier("auditTrailDaoSp")
+	private AuditTrailDao auditTrailDao;
+	// rule
 	@Autowired
 	@Qualifier("bannerRuleDaoSp")
 	private BannerRuleDao bannerRuleDao;
@@ -25,9 +34,12 @@ public class DaoSpConfigurationTest extends BaseIntegrationTest {
 	
 	@Test
 	public void testWiring() {
+		assertNotNull(ruleStatusDao);
+		assertNotNull(auditTrailDao);
+		// rule
 		assertNotNull(bannerRuleDao);
 		assertNotNull(bannerRuleItemDao);
 		assertNotNull(imagePathDao);
 	}
-	
+
 }
