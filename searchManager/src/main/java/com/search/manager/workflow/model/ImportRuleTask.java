b05ee03fb8f953ee1cfcfac536283f49dfeaffe6
mpanalig <mpanalig@pcmall.com>
2013-12-09 10:54:26 +0800

    auto-import classes

---
+++ searchManager/src/main/java/com/search/manager/workflow/model/ImportRuleTask.java
@@ -0,0 +1,106 @@
+package com.search.manager.workflow.model;
+
+import org.directwebremoting.annotations.DataTransferObject;
+import org.directwebremoting.convert.BeanConverter;
+
+import com.search.manager.enums.ImportType;
+import com.search.manager.enums.RuleType;
+import com.search.manager.model.ModelBean;
+
+@DataTransferObject(converter = BeanConverter.class)
+public class ImportRuleTask extends ModelBean{
+	
+	private static final long serialVersionUID = 8380046988342666844L;
+
+	private String taskId;
+	
+	private RuleType ruleType;
+	
+	private String sourceStoreId;
+	
+	private String sourceRuleId;
+	
+	private String sourceRuleName;
+	
+	private String targetStoreId;
+	
+	private String targetRuleId;
+	
+	private String targetRuleName;
+	
+	private ImportType importType;
+	
+	private TaskExecutionResult result;
+	
+	public String getTaskId() {
+		return taskId;
+	}
+	
+	public void setTaskId(String taskId) {
+		this.taskId = taskId;
+	}
+	
+	public RuleType getRuleType() {
+		return ruleType;
+	}
+	
+	public void setRuleType(RuleType ruleType) {
+		this.ruleType = ruleType;
+	}
+	
+	public String getSourceStoreId() {
+		return sourceStoreId;
+	}
+	
+	public void setSourceStoreId(String sourceStoreId) {
+		this.sourceStoreId = sourceStoreId;
+	}
+	
+	public String getSourceRuleId() {
+		return sourceRuleId;
+	}
+	
+	public void setSourceRuleId(String sourceRuleId) {
+		this.sourceRuleId = sourceRuleId;
+	}
+	
+	public String getSourceRuleName() {
+		return sourceRuleName;
+	}
+	
+	public void setSourceRuleName(String sourceRuleName) {
+		this.sourceRuleName = sourceRuleName;
+	}
+	
+	public String getTargetStoreId() {
+		return targetStoreId;
+	}
+	
+	public void setTargetStoreId(String targetStoreId) {
+		this.targetStoreId = targetStoreId;
+	}
+	
+	public String getTargetRuleId() {
+		return targetRuleId;
+	}
+	
+	public void setTargetRuleId(String targetRuleId) {
+		this.targetRuleId = targetRuleId;
+	}
+	
+	public String getTargetRuleName() {
+		return targetRuleName;
+	}
+	
+	public void setTargetRuleName(String targetRuleName) {
+		this.targetRuleName = targetRuleName;
+	}
+	
+	public ImportType getImportType() {
+		return importType;
+	}
+	
+	public void setImportType(ImportType importType) {
+		this.importType = importType;
+	}
+}
---
+++ searchManager/src/main/java/com/search/manager/workflow/model/TaskStatus.java
@@ -0,0 +1,54 @@
+package com.search.manager.workflow.model;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.directwebremoting.annotations.DataTransferObject;
+import org.directwebremoting.convert.EnumConverter;
+
+@DataTransferObject(
+		converter=EnumConverter.class, 
+		type="enum")
+public enum TaskStatus {
+	QUEUED("Queued", "Queued"),
+	IN_PROCESS("In Process", "In Process"),
+	FAILED("Failed", "Failed"),
+	COMPLETED("Completed", "Completed");
+
+	private final String name;
+	private final String displayText;
+	
+	private static final Map<String,TaskStatus> lookup = new HashMap<String,TaskStatus>();
+
+	TaskStatus(String name, String displayText){
+		this.name = name;
+		this.displayText = displayText;
+	}
+
+	static {
+		for(TaskStatus s : EnumSet.allOf(TaskStatus.class))
+			lookup.put(s.getDisplayText(), s);
+	}
+
+	public String getDisplayText() {
+		return displayText;
+	}
+
+	public static TaskStatus getByDisplayText(String displayText) { 
+		return lookup.get(displayText); 
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public static TaskStatus get(Integer intValue) { 
+        return TaskStatus.values()[intValue-1];
+    }
+	
+	@Override
+	public String toString() {
+		return String.valueOf(ordinal()+1);
+	}
+}
