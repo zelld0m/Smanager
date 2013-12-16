27d70d5ccc7585d8a7aa1075f17cd803eb86c040
mpanalig <mpanalig@pcmall.com>
2013-12-16 10:34:23 +0800

    added annotation

--- searchManager/src/main/java/com/search/manager/workflow/service/impl/ImportRuleTaskServiceImpl.java
+++ searchManager/src/main/java/com/search/manager/workflow/service/impl/ImportRuleTaskServiceImpl.java
@@ -1,12 +1,14 @@
 package com.search.manager.workflow.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.search.manager.dao.DaoException;
 import com.search.manager.workflow.dao.ImportRuleTaskDAO;
 import com.search.manager.workflow.model.ImportRuleTask;
 import com.search.manager.workflow.service.ImportRuleTaskService;
 
+@Service
 public class ImportRuleTaskServiceImpl implements ImportRuleTaskService{
     @Autowired private ImportRuleTaskDAO importRuleTaskDAO;
 
