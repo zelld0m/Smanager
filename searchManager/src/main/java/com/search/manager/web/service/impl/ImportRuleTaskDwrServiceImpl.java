b10e88b639bd79b6f1976f870e2f8b0670451cc6
mpanalig <mpanalig@pcmall.com>
2014-01-16 16:30:28 +0800

    updated auto import task filter qualifier for notification

--- searchManager/src/main/java/com/search/manager/web/service/impl/ImportRuleTaskDwrServiceImpl.java
+++ searchManager/src/main/java/com/search/manager/web/service/impl/ImportRuleTaskDwrServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.Predicate;
+import org.apache.commons.lang.ArrayUtils;
 import org.directwebremoting.annotations.Param;
 import org.directwebremoting.annotations.RemoteMethod;
 import org.directwebremoting.annotations.RemoteProxy;
@@ -47,7 +48,7 @@ public class ImportRuleTaskDwrServiceImpl implements ImportRuleTaskDwrService{
                 ImportRuleTask task = (ImportRuleTask) object;   
                 TaskExecutionResult result = task.getTaskExecutionResult();
                 TaskStatus taskStatus = result.getTaskStatus();
-                return taskStatus!=TaskStatus.COMPLETED;
+                return ArrayUtils.contains(TaskStatus.AWAITING_COMPLETION_STATUSES, taskStatus);
             }
         });
         
