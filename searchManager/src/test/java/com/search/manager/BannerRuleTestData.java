75b2a2e42b426e5ea4aaa1d60957743f09e29934
jjamolod <jesrome.jamolod@pcmall.com>
2014-01-22 17:32:35 +0800

    - Fixed banner rule DAO test.

--- searchManager/src/test/java/com/search/manager/BannerRuleTestData.java
+++ searchManager/src/test/java/com/search/manager/BannerRuleTestData.java
@@ -4,24 +4,24 @@ import com.search.manager.core.model.BannerRule;
 
 public class BannerRuleTestData {
 
-	public static final String DEFAULT_ID = "bannerrule1234";
-	
-	public static BannerRule getNewBannerRule() {
-		BannerRule bannerRule = new BannerRule();
-		bannerRule.setStoreId("ecost");
-		bannerRule.setRuleName("ruleName");
-		bannerRule.setCreatedBy("ecost_admin");
-		bannerRule.setComment("comment");
-		
-		return bannerRule;
-	}
-	
-	public static BannerRule getExistingBannerRule() {
-		BannerRule bannerRule = getNewBannerRule();
-		bannerRule.setRuleId(DEFAULT_ID);
-		bannerRule.setRuleName("ruleName1");
-		
-		return bannerRule;
-	}
-	
+    public static final String DEFAULT_ID = "bannerrule1234";
+
+    public static BannerRule getNewBannerRule() {
+        BannerRule bannerRule = new BannerRule();
+        bannerRule.setStoreId("ecost");
+        bannerRule.setRuleName("ruleName");
+        bannerRule.setCreatedBy("ecost_admin");
+        bannerRule.setComment("comment");
+
+        return bannerRule;
+    }
+
+    public static BannerRule getExistingBannerRule() {
+        BannerRule bannerRule = getNewBannerRule();
+        bannerRule.setRuleId(DEFAULT_ID);
+        bannerRule.setRuleName("ruleName1");
+
+        return bannerRule;
+    }
+
 }
