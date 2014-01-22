a2b68541bccefbf564723fe932085d671b815962
jjamolod <jesrome.jamolod@pcmall.com>
2014-01-22 17:30:32 +0800

    - Added CommentDaoSp and ImportRuleTaskDaoSp wiring test.

--- searchManager/src/test/java/com/search/manager/core/dao/sp/DaoSpConfigurationTest.java
+++ searchManager/src/test/java/com/search/manager/core/dao/sp/DaoSpConfigurationTest.java
@@ -10,36 +10,46 @@ import com.search.manager.core.BaseIntegrationTest;
 import com.search.manager.core.dao.AuditTrailDao;
 import com.search.manager.core.dao.BannerRuleDao;
 import com.search.manager.core.dao.BannerRuleItemDao;
+import com.search.manager.core.dao.CommentDao;
 import com.search.manager.core.dao.ImagePathDao;
+import com.search.manager.core.dao.ImportRuleTaskDao;
 import com.search.manager.core.dao.RuleStatusDao;
 
 public class DaoSpConfigurationTest extends BaseIntegrationTest {
 
-	@Autowired
-	@Qualifier("ruleStatusDaoSp")
-	private RuleStatusDao ruleStatusDao;
-	@Autowired
-	@Qualifier("auditTrailDaoSp")
-	private AuditTrailDao auditTrailDao;
-	// rule
-	@Autowired
-	@Qualifier("bannerRuleDaoSp")
-	private BannerRuleDao bannerRuleDao;
-	@Autowired
-	@Qualifier("bannerRuleItemDaoSp")
-	private BannerRuleItemDao bannerRuleItemDao;
-	@Autowired
-	@Qualifier("imagePathDaoSp")
-	private ImagePathDao imagePathDao;
-	
-	@Test
-	public void testWiring() {
-		assertNotNull(ruleStatusDao);
-		assertNotNull(auditTrailDao);
-		// rule
-		assertNotNull(bannerRuleDao);
-		assertNotNull(bannerRuleItemDao);
-		assertNotNull(imagePathDao);
-	}
+    @Autowired
+    @Qualifier("ruleStatusDaoSp")
+    private RuleStatusDao ruleStatusDao;
+    @Autowired
+    @Qualifier("auditTrailDaoSp")
+    private AuditTrailDao auditTrailDao;
+    @Autowired
+    @Qualifier("commentDaoSp")
+    private CommentDao commentDao;
+    @Autowired
+    @Qualifier("importRuleTaskDaoSp")
+    private ImportRuleTaskDao importRuleTaskDao;
+    // rule
+    @Autowired
+    @Qualifier("bannerRuleDaoSp")
+    private BannerRuleDao bannerRuleDao;
+    @Autowired
+    @Qualifier("bannerRuleItemDaoSp")
+    private BannerRuleItemDao bannerRuleItemDao;
+    @Autowired
+    @Qualifier("imagePathDaoSp")
+    private ImagePathDao imagePathDao;
+
+    @Test
+    public void testWiring() {
+        assertNotNull(ruleStatusDao);
+        assertNotNull(auditTrailDao);
+        assertNotNull(commentDao);
+        assertNotNull(importRuleTaskDao);
+        // rule
+        assertNotNull(bannerRuleDao);
+        assertNotNull(bannerRuleItemDao);
+        assertNotNull(imagePathDao);
+    }
 
 }
