1ffbec5e73763900e3f367b76fe406bbde2812cf
Angelico Barcena <angelico.barcena@onsale.com>
2014-01-24 14:37:46 +0800

    Instantiate PropertiesUtils before usage.

--- searchManager/src/test/java/com/search/manager/core/BaseIntegrationTest.java
+++ searchManager/src/test/java/com/search/manager/core/BaseIntegrationTest.java
@@ -4,17 +4,11 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.search.manager.utility.PropertiesUtils;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({
     "classpath:spring/spring-context-test.xml", "classpath:spring/spring-solr-context-test.xml",
     "classpath:spring/spring-datasource-test.xml"
 })
-public class BaseIntegrationTest {
-
-    public BaseIntegrationTest() {
-        PropertiesUtils.initPropertiesConfig("C:\\home\\solr\\conf\\globalvar.properties");
-    }
+public abstract class BaseIntegrationTest {
 
 }
